e94581c7671ecfd5375a2ae119ce9a70336be761
Joe Beernink <jobee@microsoft.com>
2021-03-25 14:38:24 -0700

    More auth changes

--- TrashMob/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrashMob.Models
-{
-    using Microsoft.AspNetCore.Identity;
-
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
