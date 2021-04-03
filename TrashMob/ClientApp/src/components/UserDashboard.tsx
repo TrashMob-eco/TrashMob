e2aafd3aafa9102f53d8b04d32b8e1fe5c823392
Joe Beernink <joe@joebeernink.com>
2021-04-03 10:59:15 -0700

    Got auth working.... almost

--- TrashMob/ClientApp/src/components/UserDashboard.tsx
+++ TrashMob/ClientApp/src/components/UserDashboard.tsx
@@ -3,29 +3,6 @@ import * as React from 'react'
 import { Link } from 'react-router-dom';
 
 export class UserDashboard extends Component {
-
-
-    private GetUserEvents(endpoint, token) {
-
-        const headers = new Headers();
-        const bearer = `Bearer ${token}`;
-
-        headers.append("Authorization", bearer);
-
-        const options = {
-            method: "GET",
-            headers: headers
-        };
-
-        fetch(endpoint, options)
-            .then(response => response.json())
-            .then(response => {
-                return response;
-            }).catch(error => {
-                console.error(error);
-            });
-    }
-
     render() {
         return (
             <div>
