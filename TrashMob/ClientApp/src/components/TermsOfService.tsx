534af16ff38fc80a352d444e853f58ecc51b22a2
Joe Beernink <joe@joebeernink.com>
2021-04-07 19:29:29 -0700

    Added confirm tos popup

--- TrashMob/ClientApp/src/components/TermsOfService.tsx
+++ TrashMob/ClientApp/src/components/TermsOfService.tsx
@@ -1,6 +1,11 @@
 ﻿import { Component } from 'react';
 import * as React from 'react'
 
+export const CurrentTermsOfServiceVersion: TermsOfServiceVersion = {
+    versionId: "0.1",
+    versionDate: new Date(2021, 4, 1, 0, 0, 0, 0)
+}
+
 export class TermsOfService extends Component {
     render() {
         return (
@@ -9,4 +14,9 @@ export class TermsOfService extends Component {
             </div>
         );
     }
+}
+
+export class TermsOfServiceVersion {
+    versionId: string = "0.1";
+    versionDate: Date = new Date(2021, 4, 1, 0, 0, 0, 0);
 }
