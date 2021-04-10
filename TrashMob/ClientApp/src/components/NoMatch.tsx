672964088318c2355fa4052de282889bc6fb59db
Joe Beernink <joe@joebeernink.com>
2021-04-10 15:05:32 -0700

    Fixed a path. Added no match to help figure out routing issue

---
+++ TrashMob/ClientApp/src/components/NoMatch.tsx
@@ -0,0 +1,12 @@
+﻿import { Component } from 'react';
+import * as React from 'react'
+
+export class NoMatch extends Component {
+    render() {
+        return (
+            <div>
+                <h1>No route matches your request</h1>
+            </div>
+        );
+    }
+}
