297097609a47550c6da5d598d57a27a498c07118
Joe Beernink <joe@joebeernink.com>
2021-05-29 09:22:12 -0700

    Added ability to send email from contact us

---
+++ TrashMob/Persistence/IEmailManager.cs
@@ -0,0 +1,12 @@
+﻿namespace TrashMob.Persistence
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TrashMob.Poco;
+
+    public interface IEmailManager
+    {
+        Task SendSystemEmail(Email email, CancellationToken cancellationToken);
+    }
+}
