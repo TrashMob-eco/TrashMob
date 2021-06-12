2d69213bc5f96acee78023827bf05972e2a86dcf
Joe Beernink <joe@joebeernink.com>
2021-06-12 11:48:19 -0700

    Added more stats

--- TrashMobNotifier/StatGenerator.cs
+++ TrashMobNotifier/StatGenerator.cs
@@ -9,7 +9,7 @@ namespace TrashMobNotifier
     public static class StatGenerator
     {
         [FunctionName("GetStats")]
-        public static async Task Run([TimerTrigger("0 0 0 */1 * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 0 */1 * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Getting Stats trigger function executed at: {DateTime.Now}");
             var connectionString = Environment.GetEnvironmentVariable("DBConnectionString");
@@ -17,18 +17,103 @@ namespace TrashMobNotifier
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                var sql = "SELECT count(*) FROM dbo.Users";
-                var numberOfUsers = 0;
-
-                using (var cmd = new SqlCommand(sql, conn))
-                {
-                    numberOfUsers = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
-
-                    log.LogInformation("There are currently '{numberOfUsers}' Users.", numberOfUsers);
-                }
-
-                await AddSiteMetrics(log, conn, "TotalSiteUsers", numberOfUsers);               
+                await CountUsers(log, conn);
+                await CountEvents(log, conn);
+                await CountEventAttendees(log, conn);
+                await CountFutureEvents(log, conn);
+                await CountFutureEventAttendees(log, conn);
+                await CountContactRequests(log, conn);
             }
+        }
+
+        private static async Task CountUsers(ILogger log, SqlConnection conn)
+        {
+            var sql = "SELECT count(*) FROM dbo.Users";
+            var numberOfUsers = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfUsers = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfUsers}' Users.", numberOfUsers);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalSiteUsers", numberOfUsers);
+        }
+
+        private static async Task CountEvents(ILogger log, SqlConnection conn)
+        {
+            var sql = "SELECT count(*) FROM dbo.Events";
+            var numberOfEvents = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfEvents = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfEvents}' Events.", numberOfEvents);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalEvents", numberOfEvents);
+        }
+
+        private static async Task CountFutureEvents(ILogger log, SqlConnection conn)
+        {
+            var sql = "SELECT count(*) FROM dbo.Events WHERE EventDate > GetDate()";
+            var numberOfEvents = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfEvents = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfEvents}' Future Events.", numberOfEvents);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalFutureEvents", numberOfEvents);
+        }
+
+        private static async Task CountEventAttendees(ILogger log, SqlConnection conn)
+        {
+            var sql = "SELECT count(*) FROM dbo.EventAttendees";
+            var numberOfEventAttendees = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfEventAttendees = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfEventAttendees}' EventAttendees.", numberOfEventAttendees);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalEventAttendees", numberOfEventAttendees);
+        }
+
+        private static async Task CountFutureEventAttendees(ILogger log, SqlConnection conn)
+        {
+            var sql = "  Select count(*) from dbo.EventAttendees ea inner join dbo.Events e on ea.EventId = e.id  WHERE e.EventDate > GetDate() ";
+            var numberOfEventAttendees = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfEventAttendees = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfEventAttendees}' EventAttendees.", numberOfEventAttendees);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalFutureEventAttendees", numberOfEventAttendees);
+        }
+
+        private static async Task CountContactRequests(ILogger log, SqlConnection conn)
+        {
+            var sql = "SELECT count(*) FROM dbo.ContactRequests";
+            var numberOfContactRequests = 0;
+
+            using (var cmd = new SqlCommand(sql, conn))
+            {
+                numberOfContactRequests = (int)await cmd.ExecuteScalarAsync().ConfigureAwait(false);
+
+                log.LogInformation("There are currently '{numberOfContactRequests}' Future Events.", numberOfContactRequests);
+            }
+
+            await AddSiteMetrics(log, conn, "TotalContactRequests", numberOfContactRequests);
         }
 
         private static async Task AddSiteMetrics(ILogger log, SqlConnection conn, string metricType, long metricValue)
