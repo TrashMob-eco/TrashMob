b40303469c74fb8c69b84087f7f820cf5fe82d67
Joe Beernink <joe@joebeernink.com>
2021-07-02 16:35:50 -0700

    Fixing startup of functions

--- TrashMobJobs/Program.cs
+++ TrashMobJobs/Program.cs
@@ -6,29 +6,31 @@ namespace TrashMobJobs
     using TrashMob.Shared;
     using TrashMob.Shared.Persistence;
     using TrashMob.Shared.Engine;
+    using System.Threading.Tasks;
 
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
-                .ConfigureServices((_, services) =>
-                    services.AddSingleton<IEmailSender, EmailSender>()
-                            .AddDbContext<MobDbContext>()
-                            .AddSingleton<IContactRequestRepository, ContactRequestRepository>()
-                            .AddSingleton<IEmailManager, EmailManager>()
-                            .AddSingleton<IEventAttendeeRepository, EventAttendeeRepository>()
-                            .AddSingleton<IEventRepository, EventRepository>()
-                            .AddSingleton<IEventStatusRepository, EventStatusRepository>()
-                            .AddSingleton<IEventTypeRepository, EventTypeRepository>()
-                            .AddSingleton<IMapRepository, MapRepository>()
-                            .AddSingleton<IUserRepository, UserRepository>()
-                            .AddSingleton<IUserNotificationRepository, UserNotificationRepository>()
-                            .AddSingleton<IUserNotificationManager, UserNotificationManager>())
+                .ConfigureServices(s =>
+                    s.AddSingleton<IEmailSender, EmailSender>()
+                     .AddDbContext<MobDbContext>()
+                     .AddSingleton<IContactRequestRepository, ContactRequestRepository>()
+                     .AddSingleton<IEmailManager, EmailManager>()
+                     .AddSingleton<IEventAttendeeRepository, EventAttendeeRepository>()
+                     .AddSingleton<IEventRepository, EventRepository>()
+                     .AddSingleton<IEventStatusRepository, EventStatusRepository>()
+                     .AddSingleton<IEventTypeRepository, EventTypeRepository>()
+                     .AddSingleton<IMapRepository, MapRepository>()
+                     .AddSingleton<IUserRepository, UserRepository>()
+                     .AddSingleton<IUserNotificationRepository, UserNotificationRepository>()
+                     .AddSingleton<IUserNotificationPreferenceRepository, UserNotificationPreferenceRepository>()
+                     .AddSingleton<IUserNotificationManager, UserNotificationManager>())
                 .Build();
 
-            host.Run();
+            await host.RunAsync().ConfigureAwait(false);
         }
     }
 }
