e7e0e0920d3fe66e43cadf4d8c8cd071cad69d55
Joe Beernink <joe@joebeernink.com>
2021-08-28 09:45:13 -0700

    Fixed react dependency issue

--- TrashMob/client-app/src/components/MyDashboard.tsx
+++ TrashMob/client-app/src/components/MyDashboard.tsx
@@ -84,7 +84,7 @@ const MyDashboard: React.FC<MyDashboardProps> = (props) => {
                     });
             });
         }
-    }, [showFutureEventsOnly, reloadEvents]);
+    }, [showFutureEventsOnly, reloadEvents, props.currentUser, props.isUserLoaded]);
 
     function handleLocationChange(point: data.Position) {
         // do nothing
