1a8798f1c43c754c293a5bd54c40dd17e4dba260
Joe Beernink <joe@joebeernink.com>
2021-09-13 20:12:23 -0700

    Updating delete user logic

---
+++ TrashMob.Shared/Migrations/20210914031121_TrashMobUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashMob.Migrations
+{
+    public partial class TrashMobUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "City", "Country", "DateAgreedToPrivacyPolicy", "DateAgreedToTermsOfService", "Email", "GivenName", "IsOptedOutOfAllEmails", "IsSiteAdmin", "Latitude", "Longitude", "MemberSince", "NameIdentifier", "PostalCode", "PrefersMetric", "PrivacyPolicyVersion", "Region", "SourceSystemUserName", "SurName", "TermsOfServiceVersion", "TravelLimitForLocalEvents", "UserName" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000000"), "Anytown", "AnyCountry", null, null, "info@trashmob.eco", "TrashMob", false, false, null, null, null, null, null, false, null, "AnyState", null, "Eco", null, 0, "TrashMob" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
