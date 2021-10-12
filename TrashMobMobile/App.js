ec1c50ab0e7f81cf6187baa394da3761fa1df54c
Joe Beernink <joe@joebeernink.com>
2021-10-12 09:59:58 -0700

    Adding reactotron

--- TrashMobMobile/App.js
+++ TrashMobMobile/App.js
@@ -3,4 +3,9 @@ import App from "./app/app.tsx"
 import { registerRootComponent } from "expo"
 
 registerRootComponent(App)
+
+if(__DEV__) {
+    import("./ReactotronConfig")
+}
+
 export default App
