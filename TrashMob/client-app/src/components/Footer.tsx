3cca0a042d34ca54bef75ffeb33ca5c30a120155
Joe Beernink <joe@joebeernink.com>
2021-10-17 19:56:11 -0700

    Fixed instagram ink

--- TrashMob/client-app/src/components/Footer.tsx
+++ TrashMob/client-app/src/components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer: React.FC = () => {
                 <a href="https://twitter.com/TrashMobEco?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @TrashMobEco</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
                 <div className="fb-like" data-href="https://www.trashmob.eco" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="true"></div>
                 <a href="https://profiles.eco/trashmob?ref=tm" rel="noopener"><img className="eco-trustmark" alt=".eco profile for trashmob.eco" src="https://trust.profiles.eco/trashmob/eco-button.svg?color=%2396BA00" /></a>
-                <a className='instagram-button' href="https://www.instagram.com/trashmob">Instagram</a>
+                <a className='instagram-button' href="https://www.instagram.com/trashmobinfo">Instagram</a>
                 <div className="g-ytsubscribe" data-channelid="UC2LgFmXFCA8kdkxd4IJ51BA" data-layout="default" data-count="default" />
             <p>
                 <Link to="/privacypolicy">Privacy Policy</Link>
