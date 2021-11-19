61fc242eb93d2ed66c845333bba757601fe0a5c0
Hsing Tang <hsingtang@microsoft.com>
2021-11-19 03:14:21 -0800

    Implement flyout menu and main page inheriting FlyoutPage. Still has invalid type casting bug.

---
+++ TrashMobMobile/TrashMobMobile/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrashMobMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : FlyoutPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            flyoutPage.listView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutMenuItem;
+            if (item != null)
+            {
+                var page = (Page)Activator.CreateInstance(item.TargetType);
+                page.Title = item.Title;
+
+                Detail = new NavigationPage(page);
+                IsPresented = false;
+
+                flyoutPage.listView.SelectedItem = null;
+            }
+        }
+    }
+}
