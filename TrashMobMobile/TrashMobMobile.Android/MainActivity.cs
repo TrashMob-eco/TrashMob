8ecc751b2373234af00946cb803979a287313d69
Joe Beernink <joe@joebeernink.com>
2021-11-18 18:49:12 -0800

    Updated paths and added app center

--- TrashMobMobile/TrashMobMobile.Android/MainActivity.cs
+++ TrashMobMobile/TrashMobMobile.Android/MainActivity.cs
@@ -12,6 +12,9 @@ namespace TrashMobMobile.Droid
     using Android.Content;
     using Android;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AppCenter;
+    using Microsoft.AppCenter.Analytics;
+    using Microsoft.AppCenter.Crashes;
 
     [Activity(Label = "TrashMobMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
@@ -39,6 +42,10 @@ namespace TrashMobMobile.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             Forms.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
+
+            AppCenter.Start("5cb8f69e-d5af-4f5c-9b7b-80fd9cc12a7d",
+                   typeof(Analytics), typeof(Crashes));
+
             LoadApplication(new App(AddServices));
         }
 
