77608d969aa98a609ea05c380e3674846c9bc398
Phil Cherner <phcherne@microsoft.com>
2021-12-30 17:28:29 -0800

    add pre-build script to insert api key env var

---
+++ TrashMobMobile/TrashMobMobile.Android/appcenter-pre-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Arguments for updating:"
+echo " - AppSecret: $GOOGLE_API_KEY"
+
+# Updating ids
+IdFile=$BUILD_REPOSITORY_LOCALPATH/TrashMobMobile/TrashMobMobile.Android/properties/AndroidManifest.xml
+
+sed -i "s/GOOGLE_API_KEY/$GOOGLE_API_KEY/g" "$IdFile"
+
+# Print out file for reference
+cat $IdFile
+
+echo "Updated secret key!"
