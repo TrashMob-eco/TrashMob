e159aa7e8ded9a7ee6ae6178311d986fecdcbed6
Phil Cherner <phcherne@microsoft.com>
2022-01-02 15:59:56 -0800

    Add blank stream

--- TrashMobMobile/TrashMobMobile.Android/appcenter-pre-build.sh
+++ TrashMobMobile/TrashMobMobile.Android/appcenter-pre-build.sh
@@ -6,7 +6,7 @@ echo " - AppSecret: $GOOGLE_API_KEY"
 # Updating ids
 IdFile=$BUILD_REPOSITORY_LOCALPATH/TrashMobMobile/TrashMobMobile.Android/properties/AndroidManifest.xml
 
-sed -i "s/GOOGLE_API_KEY/$GOOGLE_API_KEY/g" "$IdFile"
+sed -i '' "s/GOOGLE_API_KEY/$GOOGLE_API_KEY/g" "$IdFile"
 
 # Print out file for reference
 cat $IdFile
