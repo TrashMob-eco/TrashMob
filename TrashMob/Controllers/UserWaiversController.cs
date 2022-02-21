5c48f29f31516a2f9d899c1bf0b3205357e9b2ee
Joe Beernink <joe@joebeernink.com>
2022-02-21 09:31:52 -0800

    added repositories

---
+++ TrashMob/Controllers/UserWaiversController.cs
@@ -0,0 +1,94 @@
+﻿namespace TrashMob.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TrashMob.Poco;
+    using TrashMob.Shared;
+    using TrashMob.Shared.Models;
+    using TrashMob.Shared.Persistence;
+
+    [ApiController]
+    [Authorize]
+    [Route("api/userwaivers")]
+    public class UserWaiversController : ControllerBase
+    {
+        private readonly IUserWaiverRepository userWaiverRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IWaiverRepository waiverRepository;
+
+        public UserWaiversController(IUserWaiverRepository userWaiverRepository, IUserRepository userRepository, IWaiverRepository waiverRepository)
+        {
+            this.userWaiverRepository = userWaiverRepository;
+            this.userRepository = userRepository;
+            this.waiverRepository = waiverRepository;
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetUserWaivers(Guid userId, CancellationToken cancellationToken)
+        {
+            return Ok(userWaiverRepository.GetUserWaivers(cancellationToken).Where(uw => uw.UserId == userId).ToList());
+        }
+
+        [HttpGet("{userId}/{waiverId}")]
+        public IActionResult GetUserWaiver(Guid userId, Guid waiverId, CancellationToken cancellationToken)
+        {
+            var userWaiver = userWaiverRepository.GetUserWaivers(cancellationToken).FirstOrDefault(uw => uw.UserId == userId && uw.WaiverId == waiverId);
+
+            if (userWaiver == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userWaiver);
+        }
+
+        [HttpPost("{userId}/{waiverId}")]
+        public async Task<IActionResult> AddUserWaiver(Guid userId, Guid waiverId)
+        {
+            var currentUser = await userRepository.GetUserByNameIdentifier(User.FindFirst(ClaimTypes.NameIdentifier).Value).ConfigureAwait(false);
+
+            // Get the waiver details so we can calculate the expiry date.
+            var waiver = await waiverRepository.GetWaiver(waiverId);
+
+            var expiryDate = GetExpiryDate(waiver.WaiverDurationTypeId);
+
+            var userWaiver = new UserWaiver()
+            {
+                UserId = userId,
+                WaiverId = waiverId,
+                EffectiveDate = DateTimeOffset.UtcNow,
+                ExpiryDate = expiryDate,
+                CreatedByUserId = currentUser.Id,
+                LastUpdatedByUserId = currentUser.Id
+            };
+
+            await userWaiverRepository.AddUserWaiver(userWaiver).ConfigureAwait(false);
+
+            return CreatedAtAction(nameof(GetUserWaiver), new { userId, waiverId });
+        }
+
+        private DateTimeOffset GetExpiryDate(int waiverDurationId)
+        {
+            var currentDay = DateTimeOffset.Now;
+            switch ((WaiverDurationTypeEnum)waiverDurationId)
+            {
+                case WaiverDurationTypeEnum.CalendarYear:
+                    return new DateTimeOffset(currentDay.Year + 1, 1, 1, 0, 0, 0, TimeSpan.Zero);
+                case WaiverDurationTypeEnum.YearFromSigning:
+                    return currentDay.AddYears(1);
+                case WaiverDurationTypeEnum.MonthFromSigning:
+                    return currentDay.AddMonths(1);
+                case WaiverDurationTypeEnum.SingleDay:
+                    return currentDay.AddDays(1);
+                default:
+                    return DateTimeOffset.MinValue;
+            }
+        }
+    }
+}
---
+++ TrashMob/Controllers/WaiverDurationTypesController.cs
@@ -0,0 +1,27 @@
+﻿
+namespace TrashMob.Controllers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using TrashMob.Shared.Persistence;
+
+    [ApiController]
+    [Route("api/waiverdurationtypes")]
+    public class WaiverDurationTypesController : ControllerBase
+    {
+        private readonly IWaiverDurationTypeRepository waiverDurationTypeRepository;
+
+        public WaiverDurationTypesController(IWaiverDurationTypeRepository waiverDurationTypeRepository)
+        {
+            this.waiverDurationTypeRepository = waiverDurationTypeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWaiverDurationTypes(CancellationToken cancellationToken)
+        {
+            var result = await waiverDurationTypeRepository.GetAllWaiverDurationTypes(cancellationToken).ConfigureAwait(false);
+            return Ok(result);
+        }
+    }
+}
