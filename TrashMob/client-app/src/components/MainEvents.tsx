f429e4f5ea902e0c8936f9a40d5ffe37d7b64c69
Joe Beernink <joe@joebeernink.com>
2022-02-27 14:36:11 -0800

    Revert main events

--- TrashMob/client-app/src/components/MainEvents.tsx
+++ TrashMob/client-app/src/components/MainEvents.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react'
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import EventAttendeeData from './Models/EventAttendeeData';
 import { apiConfig, getDefaultHeaders, msalClient } from '../store/AuthStore';
 import { getEventType } from '../store/eventTypeHelper';
 import EventData from './Models/EventData';
 import EventTypeData from './Models/EventTypeData';
 import UserData from './Models/UserData';
-import { Button, Form, Modal } from 'react-bootstrap';
-import UserWaiverData from './Models/UserWaiverData';
+import { Button } from 'react-bootstrap';
 
 class DisplayEvent {
     id: string = "";
@@ -35,9 +34,6 @@ export interface MainEventsDataProps {
 export const MainEvents: React.FC<MainEventsDataProps> = (props) => {
     const [displayEvents, setDisplayEvents] = React.useState<DisplayEvent[]>([]);
     const history = useHistory();
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [agree, setAgree] = React.useState(false);
-    const [selectedEventId, setSelectedEventId] = React.useState("");
 
     React.useEffect(() => {
         if (props.isEventDataLoaded && props.eventList) {
@@ -63,7 +59,7 @@ export const MainEvents: React.FC<MainEventsDataProps> = (props) => {
         }
     }, [props.isEventDataLoaded, props.eventList, props.myAttendanceList, props.isUserLoaded, props.isUserEventDataLoaded])
 
-    function addAttendee() {
+    function addAttendee(eventId: string) {
 
         const account = msalClient.getAllAccounts()[0];
 
@@ -74,121 +70,42 @@ export const MainEvents: React.FC<MainEventsDataProps> = (props) => {
 
         msalClient.acquireTokenSilent(request).then(tokenResponse => {
 
-            var headers = getDefaultHeaders('GET');
+            var eventAttendee = new EventAttendeeData();
+            eventAttendee.userId = props.currentUser.id;
+            eventAttendee.eventId = eventId;
 
-            // Check to see if the user has a current TrashMob.eco waiver on file
-            fetch('api/userwaivers/current/' + props.currentUser.id, {
-                method: 'GET',
+            var data = JSON.stringify(eventAttendee);
+
+            const headers = getDefaultHeaders('POST');
+            headers.append('Authorization', 'BEARER ' + tokenResponse.accessToken);
+
+            // POST request for Add EventAttendee.  
+            fetch('/api/EventAttendees', {
+                method: 'POST',
+                body: data,
                 headers: headers,
-            }).then(response => {
-                if (response.status === 404) {
-                    // User has no signed waivers on file. Pop up waiver signing form
-                    setIsOpen(true);
-                }
-                else {
-                    var eventAttendee = new EventAttendeeData();
-                    eventAttendee.userId = props.currentUser.id;
-                    eventAttendee.eventId = selectedEventId;
-
-                    var data = JSON.stringify(eventAttendee);
-
-                    headers = getDefaultHeaders('POST');
-                    headers.append('Authorization', 'BEARER ' + tokenResponse.accessToken);
-
-                    // POST request for Add EventAttendee.  
-                    fetch('/api/EventAttendees', {
-                        method: 'POST',
-                        body: data,
-                        headers: headers,
-                    }).then((response) => response.json())
-                        .then(props.onAttendanceChanged())
-                }
-            })
+            }).then((response) => response.json())
+                .then(props.onAttendanceChanged())
         })
     }
 
     function handleAttend(eventId: string) {
 
         var accounts = msalClient.getAllAccounts();
-        setSelectedEventId(eventId);
 
         if (accounts === null || accounts.length === 0) {
             msalClient.loginRedirect().then(() => {
-                addAttendee();
+                addAttendee(eventId);
             })
         }
         else {
-            addAttendee();
+            addAttendee(eventId);
         }
-    }
-
-    function togglemodal() {
-        setIsOpen(!isOpen);
-    }
-
-    function checkboxhandler() {
-        // if agree === true, it will be set to false
-        // if agree === false, it will be set to true
-        setAgree(!agree);
-    }
-
-    function updateWaiver() {
-
-        const account = msalClient.getAllAccounts()[0];
-
-        var request = {
-            scopes: apiConfig.b2cScopes,
-            account: account
-        };
-
-        msalClient.acquireTokenSilent(request).then(tokenResponse => {
-            const headers = getDefaultHeaders('GET');
-            headers.append('Authorization', 'BEARER ' + tokenResponse.accessToken);
-
-            fetch('/api/userwaivers/' + props.currentUser.id, {
-                method: 'POST',
-                headers: headers
-            })
-                .then(response => response.json() as Promise<UserWaiverData>)
-                .then(_ => {
-                    addAttendee();
-                }
-                )
-        })
-    }
-
-    function renderAcceptWaiver() {
-        return (
-            <div>
-                <Modal isOpen={isOpen} onrequestclose={togglemodal} contentlabel="Accept Terms of Use" fade={true} style={{ width: "500px", display: "block" }}>
-                    <div className="container">
-                        <Form>
-                            <Form.Row>
-                                <Form.Group>
-                                    <Form.Label className="control-label">I have reviewed and I agree to the TrashMob.eco <Link to='./waiver'>Liability Waiver</Link>.</Form.Label>
-                                    <Form.Check id="agree" onChange={checkboxhandler} label="Yes" />
-                                </Form.Group>
-                            </Form.Row>
-                            <Form.Row>
-                                <Button disabled={!agree} className="action" onClick={() => {
-                                    updateWaiver();
-                                    togglemodal();
-                                }
-                                }>
-                                    I Agree
-                                </Button>
-                            </Form.Row>
-                        </Form>
-                    </div>
-                </Modal>
-            </div>
-        )
     }
 
     function renderEventsTable(events: DisplayEvent[]) {
         return (
             <div>
-                { renderAcceptWaiver() }
                 <table className='table table-striped' aria-labelledby="tableLabel" width='100%'>
                     <thead>
                         <tr>
