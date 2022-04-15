36da05a8139292d1b598cd61cca3dfe3d8efc714
Joe Beernink <jobee@microsoft.com>
2022-04-14 21:22:14 -0700

    converting to markers

--- TrashMob/client-app/src/components/MapControllerPointCollection.tsx
+++ TrashMob/client-app/src/components/MapControllerPointCollection.tsx
@@ -49,91 +49,57 @@ export const MapControllerPointCollection: React.FC<MapControllerProps> = (props
             });
             mapRef.sources.add(dataSourceRef);
 
-            // Create a bubble layer for rendering clustered data points.
-            var clusterBubbleLayer = new layer.BubbleLayer(dataSourceRef, "bubbleLayer", {
-                // Scale the size of the clustered bubble based on the number of points in the cluster.
-                radius: [
-                    'step',
-                    ['get', 'point_count'],
-                    20,         // Default of 20 pixel radius.
-                    100, 30,    // If point_count >= 100, radius is 30 pixels.
-                    750, 40     // If point_count >= 750, radius is 40 pixels.
-                ],
+            //// Create a bubble layer for rendering clustered data points.
+            //var clusterBubbleLayer = new layer.BubbleLayer(dataSourceRef, "bubbleLayer", {
+            //    // Scale the size of the clustered bubble based on the number of points in the cluster.
+            //    radius: [
+            //        'step',
+            //        ['get', 'point_count'],
+            //        20,         // Default of 20 pixel radius.
+            //        100, 30,    // If point_count >= 100, radius is 30 pixels.
+            //        750, 40     // If point_count >= 750, radius is 40 pixels.
+            //    ],
 
-                // Change the color of the cluster based on the value on the point_cluster property of the cluster.
-                color: [
-                    'step',
-                    ['get', 'point_count'],
-                    'lime',            //Default to lime green. 
-                    100, 'yellow',     //If the point_count >= 100, color is yellow.
-                    750, 'red'        //If the point_count >= 100, color is red.
-                ],
-                strokeWidth: 0,
-                filter: ['has', 'point_count'] //Only rendered data points which have a point_count property, which clusters do.
-            });
+            //    // Change the color of the cluster based on the value on the point_cluster property of the cluster.
+            //    color: [
+            //        'step',
+            //        ['get', 'point_count'],
+            //        'lime',            //Default to lime green. 
+            //        100, 'yellow',     //If the point_count >= 100, color is yellow.
+            //        750, 'red'        //If the point_count >= 100, color is red.
+            //    ],
+            //    strokeWidth: 0,
+            //    filter: ['has', 'point_count'] //Only rendered data points which have a point_count property, which clusters do.
+            //});
 
-            // Create a layer to render the individual locations.
-            var pointLayer = new layer.SymbolLayer(dataSourceRef, "pointLayer", {
-                filter: ['!', ['has', 'point_count']], // Filter out clustered points from this layer.   
-                iconOptions: { allowOverlap: true }
-            });
+            //// Create a layer to render the individual locations.
+            //var pointLayer = new layer.SymbolLayer(dataSourceRef, "pointLayer", {
+            //    filter: ['!', ['has', 'point_count']], // Filter out clustered points from this layer.   
+            //    iconOptions: { allowOverlap: true }
+            //});
 
-            // Create a symbol layer to render the count of locations in a cluster.
-            var clusterLayer = new layer.SymbolLayer(dataSourceRef, "clusterLayer", {
-                iconOptions: {
-                    image: 'none' //Hide the icon image.
-                },
-                textOptions: {
-                    textField: ['get', 'point_count_abbreviated'],
-                    offset: [0, 0.4]
-                }
-            });
+            //// Create a symbol layer to render the count of locations in a cluster.
+            //var clusterLayer = new layer.SymbolLayer(dataSourceRef, "clusterLayer", {
+            //    iconOptions: {
+            //        image: 'none' //Hide the icon image.
+            //    },
+            //    textOptions: {
+            //        textField: ['get', 'point_count_abbreviated'],
+            //        offset: [0, 0.4]
+            //    }
+            //});
 
-            // Add the clusterBubbleLayer and two additional layers to the map.
-            mapRef.layers.add([
-                //clusterBubbleLayer,
-                //clusterLayer,
-                pointLayer
-            ]);
-
-            mapRef.events.add('mousemove', [pointLayer], function (e) {
-
-                if (e.shapes && e.shapes.length > 0) {
-                    var feature = e.shapes[0] as data.Feature<data.Geometry, any>;
-                    var properties = feature.properties;
-
-                    var popUpHtmlContent = ReactDOMServer.renderToString(getPopUpContent(properties.eventId, properties.eventName, properties.eventDate, properties.streetAddress, properties.city, properties.region, properties.country, properties.postalCode, properties.isAttending));
-                    var popUpContent = new DOMParser().parseFromString(popUpHtmlContent, "text/xml");
-
-                    var viewDetailsButton = popUpContent.getElementById("viewDetails");
-                    if (viewDetailsButton)
-                        viewDetailsButton.addEventListener('click', function () {
-                            viewDetails(properties.eventId);
-                        });
-
-                    var addAttendeeButton = popUpContent.getElementById("addAttendee");
-                    if (addAttendeeButton)
-                        addAttendeeButton.addEventListener('click', function () {
-                            handleAttend(properties.eventId);
-                        });
-
-                    // Create a popup.
-                    var popup = new Popup({
-                        content: popUpContent.documentElement,
-                        position: e.position,
-                        pixelOffset: [0, -20],
-                        closeButton: true
-                    });
-
-                    // Open the popup.
-                    if (mapRef) {
-                        popup.open(mapRef);
-                    }
-                }
-            });
+            //// Add the clusterBubbleLayer and two additional layers to the map.
+            //mapRef.layers.add([
+            //    //clusterBubbleLayer,
+            //    //clusterLayer,
+            //    pointLayer
+            //]);
 
             props.multipleEvents.forEach(mobEvent => {
-                var position = new data.Point(new data.Position(mobEvent.longitude, mobEvent.latitude));
+
+                var position = new data.Position(mobEvent.longitude, mobEvent.latitude)
+                var point = new data.Point(position);
                 var isAtt = 'No';
                 if (props.isUserEventDataLoaded) {
                     var isAttending = props.myAttendanceList && (props.myAttendanceList.findIndex((e) => e.id === mobEvent.id) >= 0);
@@ -155,7 +121,49 @@ export const MapControllerPointCollection: React.FC<MapControllerProps> = (props
                     isAttending: isAtt,
                     name: mobEvent.name,
                 }
-                dataSourceRef.add(new data.Feature(position, properties));
+
+                dataSourceRef.add(new data.Feature(point, properties));
+
+                var marker = new HtmlMarker({
+                    position: position,
+                    draggable: false,
+                    properties: properties
+                })
+
+                mapRef.events.add('click', marker, function (e) {
+
+                    var properties = e.properties;
+
+                    var popUpHtmlContent = ReactDOMServer.renderToString(getPopUpContent(properties.eventId, properties.eventName, properties.eventDate, properties.streetAddress, properties.city, properties.region, properties.country, properties.postalCode, properties.isAttending));
+                    var popUpContent = new DOMParser().parseFromString(popUpHtmlContent, "text/xml");
+
+                    var viewDetailsButton = popUpContent.getElementById("viewDetails");
+                    if (viewDetailsButton)
+                        viewDetailsButton.addEventListener('click', function () {
+                            viewDetails(properties.eventId);
+                        });
+
+                    var addAttendeeButton = popUpContent.getElementById("addAttendee");
+                    if (addAttendeeButton)
+                        addAttendeeButton.addEventListener('click', function () {
+                            handleAttend(properties.eventId);
+                        });
+
+                    // Create a popup.
+                    var popup = new Popup({
+                        content: popUpContent.documentElement,
+                        position: e.target.position,
+                        pixelOffset: [0, -20],
+                        closeButton: true
+                    });
+
+                    // Open the popup.
+                    if (mapRef) {
+                        popup.open(mapRef);
+                    }
+                });
+
+                mapRef.markers.add(marker);
             })
 
             setIsDataSourceLoaded(true);
