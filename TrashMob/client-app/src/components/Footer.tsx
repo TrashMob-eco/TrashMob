6aba96867c00d6f9021ab086eab7277d117b71a4
Ming Ho <28768492+super-ming@users.noreply.github.com>
2022-04-20 00:17:23 -0700

    Add instagram and youtube icon to footer

--- TrashMob/client-app/src/components/Footer.tsx
+++ TrashMob/client-app/src/components/Footer.tsx
@@ -37,15 +37,32 @@ export const Footer: React.FC = () => {
                         </div>
                         <div className="col-md-4">
                             <div className="row" id="iconsOrigWrapper">
-                                <div className="iconWrapper" id="firstWrapper">
-                                    <a href={"https://www.facebook.com/trashmob.eco/"} target="_blank">
-                                        <i className="fab fa-facebook-f"></i>
-                                    </a>
-                                </div>
-                                <div className="iconWrapper">
-                                    <a href={"https://twitter.com/TrashMobEco"} target="_blank">
-                                        <i className="fab fa-twitter"></i>
-                                    </a>
+                                <div className="d-flex justify-content-between">
+                                    <div className="iconWrapper" id="firstWrapper">
+                                        <a href={"https://www.facebook.com/trashmob.eco/"} target="_blank">
+                                            <i className="fab fa-facebook-f"></i>
+                                        </a>
+                                    </div>
+                                    <div className="iconWrapper">
+                                        <a href={"https://twitter.com/TrashMobEco"} target="_blank">
+                                            <i className="fab fa-twitter"></i>
+                                        </a>
+                                    </div>
+                                    <div className="iconWrapper">
+                                        <a href={"https://profiles.eco/trashmob?ref=tm"} target="_blank" rel="noopener">
+                                            <img className="eco-trustmark" alt=".eco profile for trashmob.eco" src="https://trust.profiles.eco/trashmob/eco-button.svg?color=%2396BA00" />
+                                        </a>
+                                    </div>
+                                    <div className="iconWrapper">
+                                        <a href={"https://www.instagram.com/trashmobinfo"} target="_blank" >
+                                            <i className="fa-brands fa-instagram"></i>
+                                        </a>
+                                    </div>
+                                    <div className="iconWrapper">
+                                        <a href={"https://www.youtube.com/channel/UC2LgFmXFCA8kdkxd4IJ51BA"} target="_blank">
+                                            <i className="fa-brands fa-youtube"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
