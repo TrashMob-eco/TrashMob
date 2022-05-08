649c43474c9b7df24ab8fa9d17f745d1dcfb777d
Joe Beernink <jobee@microsoft.com>
2022-05-07 22:26:04 -0700

    Adding docusign

---
+++ TrashMob/client-app/src/components/Waivers/OAuthImplicit.js
@@ -0,0 +1,241 @@
+/**
+ * This file's functions are used for OAuthImplicit grant and
+ * related authentication operations.
+ */
+import { toast } from 'react-toastify';
+
+const oauthResponseHtml = 'oauthResponse.html'; // only used for new tab auth
+const expirationBuffer = 10 * 60; // 10 minute buffer
+const sdkString = 'codeEg_react';
+const urlFrag = '/restapi/v2.1'; // DocuSign specific
+const log = m => { console.log(m) }
+const oauthState = 'oauthState'; // The name of the localStorage item for the OAuth state parameter
+
+class OAuthImplicit {
+    //
+    // Static methods
+    //
+    /**
+     * Generate a psuedo random string
+     * See https://stackoverflow.com/a/27747377/64904
+     * @param {integer} len  length of the returned string
+     */
+    static generateId(len = 40) {
+        // dec2hex :: Integer -> String i.e. 0-255 -> '00'-'ff'
+        const arr = new Uint8Array((len || 40) / 2);
+        function dec2hex(dec) {
+            return `0${dec.toString(16)}`.substr(-2);
+        }
+        window.crypto.getRandomValues(arr);
+        return Array.from(arr, dec2hex).join('');
+    }
+
+    //
+    // constructor for the class
+    //
+    constructor(app) {
+        this.app = app;
+        this.oauthWindow = null; // only used for new tab auth
+    }
+
+    /**
+     * Handle incoming OAuth Implicit grant response
+     */
+    async receiveHash(hash) {
+        const config = window.config;
+        const accessTokenFound = hash && hash.substring(0, 14) === '#access_token=';
+        if (!accessTokenFound) { return } // EARLY RETURN
+
+        // Avoiding an injection attack: check that the hash only includes expected characters
+        // An example: #access_token=eyJ0eXA...[Access tokens can be 610 characters or longer]...wKVQLqF6A&expires_in=28800&token_type=bearer&state=e3f287fbe93...c58bd6a67fe2
+        // No characters other than #.-&=_ a-z A-Z 0-9 (no spaces)
+        const hashRegex = /[^#.\-&=_a-zA-Z0-9]/;
+        if (hash.search(hashRegex) !== -1) {
+            console.error(`Potential XSS attack via fragment (#) value: ${hash}`);
+            toast.error('Potential XSS attack via the fragment value. Please login again.', {
+                autoClose: 7000
+            });
+            return
+        }
+
+        const oauthStateValue = window.localStorage.getItem(oauthState);
+        const regex = /(#access_token=)(.*)(&expires_in=)(.*)(&token_type=)(.*)(&state=)(.*)/
+            , results = regex.exec(hash)
+            , accessToken = results[2]
+            , expiresIn = results[4]
+            , incomingState = results[8]
+            , stateOk = incomingState === oauthStateValue
+            ;
+        if (!stateOk) {
+            toast.error('State error during login. Please login again.', {
+                autoClose: 10000
+            });
+            console.error(`OAuth state mismatch!! Expected state: ${oauthStateValue}; received state: ${incomingState}`);
+            return // EARLY RETURN
+        }
+        window.localStorage.clear(); // clean up
+
+        if (config.DS_REDIRECT_AUTHENTICATION) {
+            // Using redirect the window authentication:
+            // hash was good, so erase it from the browser
+            window.history.replaceState(null, '', config.DS_APP_URL);
+        } else {
+            // Using new tab authentication:
+            // close the tab that was used for authentication
+            if (this.oauthWindow) { this.oauthWindow.close() }
+        }
+
+        // calculate expires
+        let expires = new Date()
+        expires.setTime(expires.getTime() + (expiresIn - expirationBuffer) * 1000)
+        this.accessToken = accessToken;
+
+        const toastId = toast.success('Completing the login process...', { autoClose: 7000 });
+
+        // call /oauth/userinfo for general user info
+        // This API method is common for many IdP systems.
+        // But the exact format of the response tends to vary.
+        // The following works for the DocuSign IdP.
+        const userInfo = await this.fetchUserInfo();
+        const defaultAccountArray = userInfo.accounts.filter((acc) => acc.is_default);
+        const defaultAccount = defaultAccountArray.length > 0 && defaultAccountArray[0];
+        if (!defaultAccount) {
+            const msg = `Problem: the user does not have a default account. Contact DocuSign Customer Service to fix.`;
+            log(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return;
+        }
+        // 
+        // Need to select the right proxy for the API call
+        // update the baseUri setting
+        let baseUri = config.DS_API_CORS_PROXIES[defaultAccount.base_uri];
+        if (!baseUri) {
+            const msg = `Problem: no proxy for ${defaultAccount.base_uri}.`;
+            log(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return;
+        }
+
+        const externalAccountId = await this.getExternalAccountId(
+            defaultAccount.account_id, baseUri);
+        toast.dismiss(toastId);
+        this.app.oAuthResults({
+            accessToken,
+            expires,
+            name: userInfo.name,
+            email: userInfo.email,
+            accountId: defaultAccount.account_id,
+            externalAccountId,
+            accountName: defaultAccount.account_name,
+            baseUri: baseUri,
+        })
+    }
+
+    /**
+     * Start the login flow by computing the Implicit grant URL
+     * and either redirecting to the URL for the user or
+     * creating a new browser tab for the authentication flow
+     */
+    startLogin() {
+        const config = window.config;
+        const oauthStateValue = OAuthImplicit.generateId();
+        window.localStorage.setItem(oauthState, oauthStateValue); // store for when we come back
+        let redirectUrl;
+        if (config.DS_REDIRECT_AUTHENTICATION) {
+            // Using redirect the window authentication:
+            redirectUrl = config.DS_APP_URL;
+        } else {
+            // Using new tab authentication
+            redirectUrl = `${config.DS_APP_URL}/${oauthResponseHtml}`;
+        }
+
+        const url =
+            `${window.config.DS_IDP}/oauth/auth?` +
+            `response_type=token&` +
+            `scope=${window.config.IMPLICIT_SCOPES}&` +
+            `client_id=${window.config.DS_CLIENT_ID}&` +
+            `state=${oauthStateValue}&` +
+            `redirect_uri=${encodeURIComponent(redirectUrl)}`;
+
+        if (config.DS_REDIRECT_AUTHENTICATION) {
+            // Using redirect the window authentication:
+            window.location = url;
+        } else {
+            // Using new tab authentication:
+            // Create a new tab for authentication
+            this.oauthWindow = window.open(url, "_blank");
+        }
+    }
+
+    /**
+     * logout of the DocuSign IdP. 
+     * If SSO is used, the upstream IdP may not redirect the 
+     * browser back to this app
+     */
+    logout() {
+        const config = window.config;
+        const url =
+            `${window.config.DS_IDP}/logout?` +
+            `response_type=token&` +
+            `scope=${config.IMPLICIT_SCOPES}&` +
+            `client_id=${config.DS_CLIENT_ID}&` +
+            `redirect_uri=${encodeURIComponent(config.DS_APP_URL)}&` +
+            `response_mode=logout_redirect`;
+        window.location = url;
+    }
+
+    /**
+     * A relatively common OAuth API endpoint for obtaining information
+     * on the user associated with the accessToken
+     * @returns userInfoResponse JSON 
+     */
+    async fetchUserInfo() {
+        let userInfoResponse
+        try {
+            userInfoResponse = await fetch(
+                `${window.config.DS_IDP}/oauth/userinfo`, {
+                headers: new Headers({
+                    Authorization: `Bearer ${this.accessToken}`,
+                    Accept: `application/json`,
+                    'X-DocuSign-SDK': sdkString,
+                }),
+            })
+        } catch (e) {
+            const msg = `Problem while completing login.\nPlease retry.\nError: ${e.toString()}`;
+            log(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return null;
+        }
+        if (!userInfoResponse || !userInfoResponse.ok) {
+            const msg = `Problem while completing login.\nPlease retry.\nError: ${userInfoResponse.statusText}`;
+            log(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return null;
+        }
+        return await userInfoResponse.json();
+    }
+
+    /**
+     * Fetch the user-friendly version of the accountId.
+     * See https://developers.docusign.com/docs/esign-rest-api/reference/accounts/accounts/get/
+     */
+    async getExternalAccountId(accountId, baseUri) {
+        try {
+            const url = `${baseUri}${urlFrag}/accounts/${accountId}`;
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: new Headers({
+                    Authorization: `Bearer ${this.accessToken}`,
+                    Accept: `application/json`,
+                    'X-DocuSign-SDK': sdkString,
+                })
+            });
+            const data = response && response.ok && (await response.json());
+            return data.externalAccountId;
+        } catch (e) {
+            return null;
+        }
+    }
+}
+
+export default OAuthImplicit;
---
+++ TrashMob/client-app/src/components/Waivers/Waivers.tsx
@@ -0,0 +1,467 @@
+﻿import React, { FC } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { ToastContainer, toast } from 'react-toastify';
+import OAuthImplicit from './OAuthImplicit';
+import './App.css';
+import UserData from '../Models/UserData';
+import { useEffect } from 'react';
+import DocuSign from './Docusign';
+
+export interface WaiverState {
+    accessToken: string;
+    expires: Date;
+    name: string;
+    email: string;
+    externalAccountId: string;
+    accountName: string;
+    accountId: string;
+    baseUri: string;
+    page: string;
+    working: boolean;
+    workingMessage: string;
+    responseErrorMsg: string;
+    responseEnvelopeId: string;
+    responseAvailableApiRequests: number;
+    responseApiRequestsReset: Date;
+    responseSuccess: boolean;
+    responseTraceId: string;
+    resultsEnvelopeJson: string;
+    formName: string;
+    formEmail: string;
+}
+
+export interface WaiversProps {
+    isUserLoaded: boolean;
+    currentUser: UserData;
+    waiverState: WaiverState;
+};
+
+const Waivers: React.FC<WaiversProps> = (props) => {
+
+    const oAuthImplicit = new OAuthImplicit(this);
+    const docusign = new DocuSign(this);
+    const [accessToken, setAccessToken] = React.useState<string | undefined>(props.waiverState.accessToken);
+    const [expires, setExpires] = React.useState<Date | undefined>(props.waiverState.expires);
+    const [name, setName] = React.useState<string | undefined>(props.waiverState.name);
+    const [email, setEmail] = React.useState<string | undefined>(props.waiverState.email);
+    const [externalAccountId, setExternalAccountId] = React.useState<string | undefined>(props.waiverState.externalAccountId);
+    const [accountName, setAccountName] = React.useState<string | undefined>(props.waiverState.accountName);
+    const [accountId, setAccountId] = React.useState<string | undefined>(props.waiverState.accountId);
+    const [baseUri, setBaseUri] = React.useState<string | undefined>(props.waiverState.baseUri);
+    const [page, setPage] = React.useState<string>('welcome');
+    const [working, setWorking] = React.useState<boolean>(false);
+    const [workingMessage, setWorkingMessage] = React.useState<string>('');
+    const [responseErrorMsg, setResponseErrorMsg] = React.useState<string | undefined>(props.waiverState.accountId);
+    const [responseEnvelopeId, setResponseEnvelopeId] = React.useState<string | undefined>(props.waiverState.responseEnvelopeId);
+    const [responseAvailableApiRequests, setResponseAvailableApiRequests] = React.useState<number | undefined>(props.waiverState.responseAvailableApiRequests);
+    const [responseApiRequestsReset, setResponseApiRequestsReset] = React.useState<Date | undefined>(props.waiverState.responseApiRequestsReset);
+    const [responseSuccess, setResponseSuccess] = React.useState<boolean | undefined>(props.waiverState.responseSuccess);
+    const [responseTraceId, setResponseTraceId] = React.useState<string | undefined>(props.waiverState.responseTraceId);
+    const [resultsEnvelopeJson, setResultsEnvelopeJson] = React.useState<string | undefined>(props.waiverState.resultsEnvelopeJson);
+    const [formName, setFormName] = React.useState<string>('');
+    const [formEmail, setFormEmail] = React.useState<string>('');
+
+    useEffect(() => {
+        /**
+         * Starting up--if our URL includes a hash, check it to see if
+         * it's the OAuth response
+         */
+        const config = window.config;
+        // if the url has a query parameter of ?error=logout_request (from a logout operation) 
+        // then remove it
+        if (window.location.search && window.location.search === '?error=logout_request') {
+            window.history.replaceState(null, '', config.DS_APP_URL);
+        }
+
+        if (config?.DS_REDIRECT_AUTHENTICATION) {
+            const hash = window.location.hash;
+            if (!hash) { return }
+            // possible OAuth response
+            setWorking(true);
+            setWorkingMessage('Logging in');
+            oAuthImplicit.receiveHash(hash)
+                .then(() => {
+                    setWorking(false);
+                });
+        } else {
+            // await authentication via the new tab
+            window.addEventListener("message", receiveMessage, false);
+        }
+    });
+
+    /**
+     * Receive message from a child .
+     * This method is only used if authentication is done
+     * in a new tab. See file public/oauthResponse.html
+     * @param {object} e 
+     */
+    async function receiveMessage(e: any) {
+        const rawSource = e && e.data && e.data.source
+            , ignore = {
+                'react-devtools-inject-backend': true,
+                'react-devtools-content-script': true,
+                'react-devtools-detector': true,
+                'react-devtools-bridge': true
+            }
+            , source = (rawSource && !ignore[rawSource]) ? rawSource : false
+            ;
+        if (!source) { return }; // Ignore if no source field
+        if (source === 'oauthResponse') {
+            setWorking(true);
+            setWorkingMessage('Logging in');
+            const hash = e.data && e.data.hash;
+            await oAuthImplicit.receiveHash(hash);
+            setWorking(false);
+        }
+    }
+
+    function startAuthentication() {
+        oAuthImplicit.startLogin();
+    }
+
+    /**
+     * Is the accessToken ok to use?
+     * @returns boolean accessTokenIsGood
+     */
+    function checkToken() {
+        if (
+            !accessToken ||
+            expires === undefined ||
+            new Date() > expires
+        ) {
+            // Need new login. Only clear auth, don't clear the state (leave form contents);
+            clearAuth();
+            setPage('welcome');
+            setWorking(false);
+            toast.error('Your login session has ended.\nPlease login again', {
+                autoClose: 8000,
+            });
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method clears this app's authentication information.
+     * But there may still be an active login session cookie
+     * from the IdP. Your IdP may have an API method for clearing
+     * the login session.
+     */
+    function logout() {
+        clearAuth();
+        clearState();
+        setPage('welcome');
+        toast.success('You have logged out.', { autoClose: 5000 });
+        oAuthImplicit.logout();
+    }
+
+    /**
+     * Clear authentication-related state
+     */
+    function clearAuth() {
+        setAccessToken(undefined);
+        setExpires(undefined);
+        setAccountId(undefined);
+        setExternalAccountId(undefined);
+        setAccountName(undefined);
+        setBaseUri(undefined);
+        setName(undefined);
+        setEmail(undefined);
+    }
+
+    /**
+     * Clear the app's form and related state
+     */
+    function clearState() {
+        setFormName('');
+        setFormEmail('');
+        setWorking(false);
+        setResponseErrorMsg(undefined);
+        setResponseEnvelopeId(undefined);
+        setResponseAvailableApiRequests(undefined);
+        setResponseApiRequestsReset(undefined)
+        setResponseSuccess(undefined);
+        setResponseTraceId(undefined);
+        setResultsEnvelopeJson(undefined);
+    }
+
+    /**
+     * Process the oauth results.
+     * This method is called by the OAuthImplicit class
+     * @param results
+     */
+    function oAuthResults(results: any) {
+        setAccessToken(results.accessToken);
+        setExpires(results.expires);
+        setName(results.name);
+        setExternalAccountId(results.externalAccountId);
+        setEmail(results.email);
+        setAccountId(results.accountId);
+        setAccountName(results.accountName);
+        setBaseUri(results.baseUri);
+        setPage('loggedIn');
+        setFormName(results.name);
+        setFormEmail(results.email);
+
+        toast.success(`Welcome ${results.name}, you are now logged in`);
+    }
+
+    function formNameChange(event: any) {
+        setFormName(event.target.value);
+    }
+
+    function formEmailChange(event: any) {
+        setFormEmail(event.target.value);
+    }
+
+    async function sendEnvelope() {
+        setResponseErrorMsg(undefined);
+        setResponseEnvelopeId(undefined);
+        setResponseAvailableApiRequests(undefined);
+        setResponseApiRequestsReset(undefined);
+        setResponseSuccess(undefined);
+        setResponseTraceId(undefined);
+        setResultsEnvelopeJson(undefined);
+
+        if (!checkToken()) {
+            return; // Problem! The user needs to login
+        }
+
+        if (!formEmail || formEmail.length < 5) {
+            toast.error("Problem: Enter the signer's email address");
+            return;
+        }
+
+        if (!formName || formName.length < 5) {
+            toast.error("Problem: Enter the signer's name");
+            return;
+        }
+
+        setWorking(true);
+        setWorkingMessage("Sending envelope");
+        const results = await docusign.sendEnvelope();
+
+        if (!results) {
+            return;
+        }
+
+        const { apiRequestsReset } = results;
+        const responseApiRequestsReset = apiRequestsReset ? new Date(apiRequestsReset) : undefined;
+        setWorking(false);
+        setResponseSuccess(results.success);
+        setResponseErrorMsg(results.errorMsg);
+        setResponseEnvelopeId(results.envelopeId);
+        setResponseAvailableApiRequests(results.availableApiRequests);
+        setResponseTraceId(results.traceId);
+        setResponseApiRequestsReset(responseApiRequestsReset);
+    }
+
+    async function getEnvelope() {
+        setResponseErrorMsg(undefined);
+        setResponseEnvelopeId(undefined);
+        setResponseAvailableApiRequests(undefined);
+        setResponseApiRequestsReset(undefined);
+        setResponseSuccess(undefined);
+        setResponseTraceId(undefined);
+
+        if (!checkToken()) {
+            return; // Problem! The user needs to login
+        }
+        if (!responseEnvelopeId) {
+            toast.error("Problem: First send an envelope");
+            return;
+        }
+
+        setWorking(true);
+        setWorkingMessage("Fetching the envelope's status");
+
+        const results = await docusign.getEnvelope();
+        const { apiRequestsReset } = results;
+        const responseApiRequestsReset = apiRequestsReset ? new Date(apiRequestsReset) : undefined;
+
+        setWorking(false);
+        setResponseSuccess(results.success);
+        setResponseErrorMsg(results.errorMsg);
+        setResponseAvailableApiRequests(results.availableApiRequests);
+        setResponseTraceId(results.traceId);
+        setResponseApiRequestsReset(responseApiRequestsReset);
+        setResultsEnvelopeJson(results.resultsEnvelopeJson);
+        setResponseApiRequestsReset(responseApiRequestsReset);
+    }
+
+    function LoggedIn() {
+        const resetTime = responseApiRequestsReset;
+        const resetTimeString = resetTime
+            ? new Intl.DateTimeFormat('en-US', {
+                dateStyle: 'medium',
+                timeStyle: 'full',
+            }).format(resetTime)
+            : undefined;
+        return (
+            <Container className='bodyMargin'>
+                <Row>
+                    <Col className="col-md-4">
+                        <h2>Send an Envelope</h2>
+                        <Form>
+                            <Form.Group controlId="formName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" placeholder="Name"
+                                    value={formName}
+                                    onChange={formNameChange}
+
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Email"
+                                    value={formEmail}
+                                    onChange={formEmailChange}
+                                />
+                            </Form.Group>
+
+                            <Button variant="primary" onClick={sendEnvelope}>
+                                Send Envelope
+                            </Button>
+                            <Button variant="primary" className='ml-4' onClick={getEnvelope}>
+                                Get Envelope Status
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row className='mt-4'>
+                    <Col>
+                        <h2>Results</h2>
+                        <h2>
+                            {responseSuccess !== undefined ? (
+                                responseSuccess ? (
+                                    <>✅ Success!</>
+                                ) : (
+                                    <>❌ Problem!</>
+                                )
+                            ) : null}
+                        </h2>
+                        {responseErrorMsg ? (
+                            <p>Error message: {responseErrorMsg}</p>
+                        ) : null}
+                        {responseEnvelopeId ? (
+                            <p>Envelope ID: {responseEnvelopeId}</p>
+                        ) : null}
+                        {resultsEnvelopeJson ? (
+                            <p><pre>Response: {JSON.stringify(resultsEnvelopeJson, null, 4)}</pre></p>
+                        ) : null}
+                        {responseAvailableApiRequests ? (
+                            <p>
+                                Available API requests: {responseAvailableApiRequests}
+                            </p>
+                        ) : null}
+                        {resetTimeString ? (
+                            <p>API requests reset time: {resetTimeString}</p>
+                        ) : null}
+                        {responseTraceId ? (
+                            <p>
+                                Trace ID: {responseTraceId}. Please include with all
+                                customer service questions.
+                            </p>
+                        ) : null}
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+
+    function Welcome() {
+        return (
+            <Container fluid className='welcomeMargin'>
+                <Row>
+                    <Col>
+                        <Jumbotron>
+                            <h1>React Example with OAuth Authentication</h1>
+                            <p>
+                                In this example the user authenticates with DocuSign via the OAuth Implicit grant flow.
+                                Since the app will then have an access token for the user, the app can call any
+                                DocuSign eSignature REST API method.
+                            </p>
+                            <p>
+                                Use this example for apps used by the staff of your organization who have
+                                DocuSign accounts. For example, an application could pull data from multiple
+                                sources and then send an envelope that includes the data.
+                            </p>
+                            <p>
+                                Login with your DocuSign Developer (Demo) credentials.
+                            </p>
+                            <p>
+                                <Button variant="primary" onClick={startAuthentication}>Login</Button>
+                            </p>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+
+    // Just two pages with a common header. 
+    // Choose the body of the page:
+    let pagebody;
+    switch (page) {
+        case 'welcome': // not logged in
+            pagebody = Welcome();
+            break;
+        case 'loggedIn':
+            pagebody = LoggedIn();
+            break;
+        default:
+            pagebody = Welcome();
+    };
+
+    // Compute the name block for the top nav section
+    let nameBlock;
+    if (accessToken) {
+        nameBlock = (
+            <Navbar.Text>
+                {name}<br />
+                {accountName} ({externalAccountId})
+                <Nav>
+                    <Nav.Link href="#" onClick={() => logout()}>Logout</Nav.Link>
+                </Nav>
+            </Navbar.Text>
+        )
+    } else {
+        nameBlock = null;
+    }
+
+    // The spinner
+    const spinner = (
+        <Container fluid className='bodyMargin'
+            style={{ display: working ? 'block' : 'none' }}>
+            <Row className='justify-content-center'>
+                <div className="spinner" />
+            </Row>
+            <Row className='justify-content-center'>
+                <h3>{workingMessage}…</h3>
+            </Row>
+        </Container>
+    )
+
+    // The complete page:
+    return (
+        <>
+            <Navbar fixed="top" bg="primary" variant="dark" >
+                <Navbar.Brand>DocuSign Code Example</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">{nameBlock}</Navbar.Collapse>
+            </Navbar>
+            <ToastContainer />
+            {spinner}
+            {pagebody}
+        </>
+    );
+}
+
+export default Waivers;
