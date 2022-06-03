f06d9d1458fd35e592c1f52e294daed89412e400
Joe Beernink <jobee@microsoft.com>
2022-06-02 22:05:07 -0700

    More formatting checks

--- TrashMob.Shared/Persistence/DocusignManager.cs
+++ TrashMob.Shared/Persistence/DocusignManager.cs
@@ -36,8 +36,8 @@ namespace TrashMob.Shared.Persistence
             accountId = configuration["DocusignAccountId"];
             basePath = configuration["DocusignBasePath"];
             redirectHome = configuration["DocusignRedirectHome"];
-            privateKey = privateKey.Replace("-----BEGIN RSA PRIVATE KEY-----", "-----BEGIN RSA PRIVATE KEY-----\n");
-            privateKey = privateKey.Replace("-----END RSA PRIVATE KEY-----", "\n-----END RSA PRIVATE KEY-----");
+            privateKey = privateKey.Replace("-----BEGIN RSA PRIVATE KEY----- ", "-----BEGIN RSA PRIVATE KEY-----\n");
+            privateKey = privateKey.Replace(" -----END RSA PRIVATE KEY-----", "\n-----END RSA PRIVATE KEY-----\n");
         }
 
         public EnvelopeResponse SendEnvelope(EnvelopeRequest envelopeRequest)
