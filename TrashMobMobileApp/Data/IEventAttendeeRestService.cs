ef33a6b009971874096dfc102442980042b405e5
Joe Beernink <jobee@microsoft.com>
2022-08-29 11:32:06 -0700

    Got data basics working

---
+++ TrashMobMobileApp/Data/IEventAttendeeRestService.cs
@@ -0,0 +1,11 @@
+﻿namespace TrashMobMobileApp.Data
+{
+    using System.Threading.Tasks;
+    using TrashMobMobileApp.Models;
+
+    public interface IEventAttendeeRestService
+    {
+        Task AddAttendeeAsync(EventAttendee eventAttendee);
+        Task RemoveAttendeeAsync(EventAttendee eventAttendee);
+    }
+}
--- TrashMobMobileApp/Platforms/Android/MainApplication.cs
+++ TrashMobMobileApp/Platforms/Android/MainApplication.cs
@@ -1,15 +1,15 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace TrashMobMobileApp;
+namespace TrashMobMobileApp.Platforms.Android;
 
 [Application]
 public class MainApplication : MauiApplication
 {
-	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-		: base(handle, ownership)
-	{
-	}
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
