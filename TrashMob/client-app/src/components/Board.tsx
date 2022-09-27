ce2ad4677afbd9a84e737a37a145fc6f0eeb1e3a
Joe Beernink <jobee@microsoft.com>
2022-09-27 16:00:26 -0700

    Update board to current list

--- TrashMob/client-app/src/components/Board.tsx
+++ TrashMob/client-app/src/components/Board.tsx
@@ -8,13 +8,13 @@ export const Board: React.FC = () => {
                 <ul>
                     <li><b>President:</b>  Joe Beernink</li>
                     <li><b>Vice-President:</b> Mike Rosen</li>
-                    <li><b>Treasurer:</b> Terri Register</li>
-                    <li><b>Secretary:</b> Open</li>
+                    <li><b>Treasurer:</b> Open</li>
+                    <li><b>Secretary:</b> Terri Register</li>
                     <li><b>Member:</b> Jake Dilberto</li>
                     <li><b>Member:</b> Jeremiah Steen</li>
-                    <li><b>Member:</b> Tom Turchiano</li>
                     <li><b>Member:</b> Darryl Walter</li>
-                </ul>
+                    <li><b>Member:</b> Kevin Gleason</li>
+                </ul>1
             </p>
         </div>
     );
