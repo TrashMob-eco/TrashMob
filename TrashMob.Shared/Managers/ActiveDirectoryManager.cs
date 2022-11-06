375668c3f0ee201eee6a9fe3d842a410475e9d29
Joe Beernink <jobee@microsoft.com>
2022-11-06 14:37:07 -0800

    Fixed build issues

--- TrashMob.Shared/Managers/ActiveDirectoryManager.cs
+++ TrashMob.Shared/Managers/ActiveDirectoryManager.cs
@@ -23,10 +23,10 @@ namespace TrashMob.Shared.Managers
         {
             User originalUser;
 
-            if ((originalUser = await userManager.UserExistsAsync(activeDirectoryNewUserRequest.Identities[0].issuerAssignedId, cancellationToken).ConfigureAwait(false)) != null)
+            if ((originalUser = await userManager.UserExistsAsync(activeDirectoryNewUserRequest.identities[0].issuerAssignedId, cancellationToken).ConfigureAwait(false)) != null)
             {
-                originalUser.Email = activeDirectoryNewUserRequest.Email;
-                originalUser.SourceSystemUserName = activeDirectoryNewUserRequest.Identities[0].issuerAssignedId;
+                originalUser.Email = activeDirectoryNewUserRequest.email;
+                originalUser.SourceSystemUserName = activeDirectoryNewUserRequest.identities[0].issuerAssignedId;
 
                 await userManager.UpdateAsync(originalUser, cancellationToken).ConfigureAwait(false);
 
@@ -42,19 +42,19 @@ namespace TrashMob.Shared.Managers
 
             var user = new User
             {
-                Email = activeDirectoryNewUserRequest.Email,
-                GivenName = activeDirectoryNewUserRequest.GivenName,
-                SurName = activeDirectoryNewUserRequest.Surname,
-                UserName = activeDirectoryNewUserRequest.DisplayName
+                Email = activeDirectoryNewUserRequest.email,
+                GivenName = activeDirectoryNewUserRequest.givenName,
+                SurName = activeDirectoryNewUserRequest.surname,
+                UserName = activeDirectoryNewUserRequest.displayName
             };
 
-            if (string.IsNullOrEmpty(activeDirectoryNewUserRequest.DisplayName))
+            if (string.IsNullOrEmpty(activeDirectoryNewUserRequest.displayName))
             {
                 // On insert we need a random user name to avoid duplicates, but we don't want to show the full email address ever, so take a subset
                 // of their email and then add a random number to the end.
                 Random rnd = new();
                 var userNum = rnd.Next(100, 999).ToString();
-                var first = activeDirectoryNewUserRequest.Email.Split("@")[0];
+                var first = activeDirectoryNewUserRequest.email.Split("@")[0];
                 user.UserName = first.Substring(0, Math.Min(first.Length - 1, 8)) + userNum;
             }
 
