025945f7e35bd84cfcdc44406334ad4ca86d5e65
kiran <kiranm0697@gmail.com>
2022-11-10 18:08:29 +0530

    Updated UX for Event Dashboard to match designs.

--- TrashMob/client-app/src/components/EventManagement/ManageEventAttendees.tsx
+++ TrashMob/client-app/src/components/EventManagement/ManageEventAttendees.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import { apiConfig, getDefaultHeaders, msalClient } from '../../store/AuthStore';
 import UserData from '../Models/UserData';
 import { Guid } from 'guid-typescript';
+import { Button, Container, Dropdown } from 'react-bootstrap';
+import { Envelope, PersonX, ThreeDots } from 'react-bootstrap-icons';
+import { Modal, ModalBody } from 'reactstrap';
 
 export interface ManageEventAttendeesProps {
     eventId: string;
@@ -10,8 +13,31 @@ export interface ManageEventAttendeesProps {
 }
 
 export const ManageEventAttendees: React.FC<ManageEventAttendeesProps> = (props) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [modalTitle, setModalTitle] = React.useState("")
+    const [modalDescription, setModalDescription] = React.useState("")
     const [eventAttendees, setEventAttendees] = React.useState<UserData[]>([]);
     const [isEventAttendeeDataLoaded, setIsEventAttendeeDataLoaded] = React.useState<boolean>(false);
+
+    const togglemodal = () => {
+        setIsOpen(!isOpen);
+    }
+    const messageToAttendee = () => {
+        togglemodal()
+        setModalTitle("Message to [username here]")
+        setModalDescription("Maybe I want to send a message to a specific person about something idk. Well that message would be sent here!")
+    }
+
+    const removeToAttendee = () => {
+        togglemodal()
+        setModalTitle("Remove [username]?")
+        setModalDescription("Are you sure you want to remove attendee [username]? They will be removed from the attendees list for this event and will not receive any event updates or emails. This action cannot be undone.")
+    }
+    const messageToAll = () => {
+        togglemodal()
+        setModalTitle("Message all attendees")
+        setModalDescription("Maybe I want to send a specific message out to all the attendees. Maybe it’s reminding them of what clothes to wear, that the event time changed, etc. ")
+    }
 
     React.useEffect(() => {
         if (props.isUserLoaded && props.eventId && props.eventId !== Guid.EMPTY) {
@@ -45,19 +71,32 @@ export const ManageEventAttendees: React.FC<ManageEventAttendeesProps> = (props)
                 <table className='table table-striped' aria-labelledby="tableLabel">
                     <thead>
                         <tr>
-                            <th>User Name</th>
-                            <th>City</th>
-                            <th>Country</th>
-                            <th>Member Since</th>
+                            <th className='h5 py-4'>User Name</th>
+                            <th className='h5 py-4'>City</th>
+                            <th className='h5 py-4'>Country</th>
+                            <th className='h5 py-4'>Join date</th>
+                            <th className='h5 py-4'>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {users.map(user =>
                             <tr key={user.id.toString()}>
-                                <td>{user.userName ? user.userName : user.sourceSystemUserName}</td>
-                                <td>{user.city}</td>
-                                <td>{user.country}</td>
-                                <td>{new Date(user.memberSince).toLocaleDateString()}</td>
+                                <td className='color-grey p-18 py-3'>{user.userName ? user.userName : user.sourceSystemUserName}</td>
+                                <td className='color-grey p-18 py-3'>{user.city}</td>
+                                <td className='color-grey p-18 py-3'>{user.country}</td>
+                                <td className='color-grey p-18 py-3'>{new Date(user.memberSince).toLocaleDateString()}</td>
+                                <td className='color-grey p-18 py-3'>
+                                    <Dropdown>
+                                        <Dropdown.Toggle id="userBtn" variant="light" className='remove-drop-icon'>
+                                            <ThreeDots size={24} color="#696B72" />
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu className="shadow border-0">
+                                            <Dropdown.Item eventKey="1" onClick={() => messageToAttendee()}><Envelope aria-hidden="true" color='#96ba00' size={24} className="mr-2" /><span className='color-grey p-18'>Message attendee</span></Dropdown.Item>
+                                            <Dropdown.Divider />
+                                            <Dropdown.Item eventKey="2" onClick={() => removeToAttendee()}><PersonX aria-hidden="true" color='#96ba00' size={24} className="mr-2" /><span className='color-grey p-18'>Remove attendee</span></Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </td>
                             </tr>
                         )}
                     </tbody>
@@ -68,11 +107,39 @@ export const ManageEventAttendees: React.FC<ManageEventAttendeesProps> = (props)
 
     return (
         <>
-            <div>
+            <Modal isOpen={isOpen} centered onrequestclose={togglemodal} contentlabel="Delete Account?" fade={true} size={"lg"}>
+                <ModalBody>
+                    <h2 className='fw-500'>{modalTitle}</h2>
+
+                    <p className='p-18'>{modalDescription}</p>
+                    <div className='d-flex justify-content-end'>
+                        <Button className="action h-49 p-18" onClick={() => {
+                            togglemodal();
+                        }
+                        }>
+                            Cancel
+                        </Button>
+                        <Button variant="outline" className='ml-2 border-danger text-danger h-49' onClick={() => {
+                            togglemodal();
+                        }
+                        }>
+                            Delete
+                        </Button>
+                    </div>
+                </ModalBody>
+            </Modal>
+            <Container className='p-4 bg-white rounded my-5'>
+                <div className='d-flex align-items-center justify-content-between'>
+                    <h4 className='fw-600 color-primary my-4'>Attendees ({eventAttendees?.length})</h4>
+                    <div className='d-flex align-items-center' onClick={() => messageToAll()} role="button">
+                        <Envelope aria-hidden="true" size={24} color="#96ba00" />
+                        <div className='p-18 color-primary ml-2'>Message all</div>
+                    </div>
+                </div>
                 {props.eventId === Guid.EMPTY && <p> <em>Event must be created first.</em></p>}
                 {!isEventAttendeeDataLoaded && props.eventId !== Guid.EMPTY && <p><em>Loading...</em></p>}
                 {isEventAttendeeDataLoaded && renderEventAttendeesTable(eventAttendees)}
-            </div>
+            </Container>
         </>
     );
 }
--- TrashMob/client-app/src/components/EventManagement/ManageEventPartners.tsx
+++ TrashMob/client-app/src/components/EventManagement/ManageEventPartners.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { apiConfig, getDefaultHeaders, msalClient } from '../../store/AuthStore';
 import UserData from '../Models/UserData';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Dropdown } from 'react-bootstrap';
 import * as Constants from '../Models/Constants';
 import DisplayEventPartnerLocationData from '../Models/DisplayEventPartnerLocationData';
 import EventPartnerLocationServiceStatusData from '../Models/EventPartnerLocationServiceStatusData';
@@ -11,6 +11,7 @@ import { getServiceType } from '../../store/serviceTypeHelper';
 import ServiceTypeData from '../Models/ServiceTypeData';
 import EventPartnerLocationServiceData from '../Models/EventPartnerLocationServiceData';
 import DisplayEventPartnerLocationServiceData from '../Models/DisplayEventPartnerLocationServiceData';
+import { Building, Envelope, Eye, ThreeDots } from 'react-bootstrap-icons';
 
 export interface ManageEventPartnersProps {
     eventId: string;
@@ -101,30 +102,30 @@ export const ManageEventPartners: React.FC<ManageEventPartnersProps> = (props) =
         });
     }
 
-    function handleViewPartnerServices(locationId: string) {
-        const account = msalClient.getAllAccounts()[0];
+    // function handleViewPartnerServices(locationId: string) {
+    //     const account = msalClient.getAllAccounts()[0];
 
-        var request = {
-            scopes: apiConfig.b2cScopes,
-            account: account
-        };
+    //     var request = {
+    //         scopes: apiConfig.b2cScopes,
+    //         account: account
+    //     };
 
-        msalClient.acquireTokenSilent(request).then(tokenResponse => {
-            const headers = getDefaultHeaders('GET');
-            headers.append('Authorization', 'BEARER ' + tokenResponse.accessToken);
+    //     msalClient.acquireTokenSilent(request).then(tokenResponse => {
+    //         const headers = getDefaultHeaders('GET');
+    //         headers.append('Authorization', 'BEARER ' + tokenResponse.accessToken);
 
-            fetch('/api/eventpartnerlocationservices/' + props.eventId + "/" + locationId, {
-                method: 'GET',
-                headers: headers,
-            })
-                .then(response => response.json() as Promise<DisplayEventPartnerLocationServiceData[]>)
-                .then(data => {
-                    setPartnerLocationId(locationId);
-                    setEventPartnerLocationServices(data);
-                    setIsPartnerLocationServicesDataLoaded(true);
-                });
-        });
-    }
+    //         fetch('/api/eventpartnerlocationservices/' + props.eventId + "/" + locationId, {
+    //             method: 'GET',
+    //             headers: headers,
+    //         })
+    //             .then(response => response.json() as Promise<DisplayEventPartnerLocationServiceData[]>)
+    //             .then(data => {
+    //                 setPartnerLocationId(locationId);
+    //                 setEventPartnerLocationServices(data);
+    //                 setIsPartnerLocationServicesDataLoaded(true);
+    //             });
+    //     });
+    // }
 
     // This will handle Cancel button click event.
     function handleCancel(event: any) {
@@ -199,21 +200,34 @@ export const ManageEventPartners: React.FC<ManageEventPartnersProps> = (props) =
                 <table className='table table-striped' aria-labelledby="tableLabel" width='100%'>
                     <thead>
                         <tr>
-                            <th>Partner Name</th>
-                            <th>Partner Location Name</th>
-                            <th>Partner Notes</th>
-                            <th>Partner Services Engaged</th>
+                            <th className='h5 py-4'>Partner Name</th>
+                            <th className='h5 py-4'>Location</th>
+                            <th className='h5 py-4'>Notes</th>
+                            <th className='h5 py-4'>Status</th>
+                            <th className='h5 py-4'>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {eventPartnerLocations.map(eventPartnerLocation =>
                             <tr key={eventPartnerLocation.partnerLocationId.toString()}>
-                                <td>{eventPartnerLocation.partnerName}</td>
-                                <td>{eventPartnerLocation.partnerLocationName}</td>
-                                <td>{eventPartnerLocation.partnerLocationNotes}</td>
-                                <td>{eventPartnerLocation.partnerServicesEngaged}</td>
-                                <td>
+                                <td className='color-grey p-18 py-3'>{eventPartnerLocation.partnerName}</td>
+                                <td className='color-grey p-18 py-3'>{eventPartnerLocation.partnerLocationName}</td>
+                                <td className='color-grey p-18 py-3'>{eventPartnerLocation.partnerLocationNotes}</td>
+                                <td className='color-grey p-18 py-3'>{eventPartnerLocation.partnerServicesEngaged}</td>
+                                {/* <td>
                                     <Button className="action" onClick={() => handleViewPartnerServices(eventPartnerLocation.partnerLocationId)}>View Partner Services</Button>
+                                </td> */}
+                                <td className='color-grey p-18 py-3'>
+                                    <Dropdown>
+                                        <Dropdown.Toggle id="userBtn" variant="light" className='remove-drop-icon'>
+                                            <ThreeDots size={24} color="#696B72" />
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu className="shadow border-0">
+                                            <Dropdown.Item eventKey="1" href="/mydashboard"><Envelope aria-hidden="true" color='#96ba00' size={24} className="mr-2" /><span className='color-grey p-18'>Message partner</span></Dropdown.Item>
+                                            <Dropdown.Divider />
+                                            <Dropdown.Item eventKey="2" href="/manageeventdashboard"><Eye aria-hidden="true" color='#96ba00' size={24} className="mr-2" /><span className='color-grey p-18'>View partner request</span></Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
                                 </td>
                             </tr>
                         )}
@@ -254,14 +268,19 @@ export const ManageEventPartners: React.FC<ManageEventPartnersProps> = (props) =
     }
 
     return (
-        <>
-            <div>
-                {props.eventId === Guid.EMPTY && <p> <em>Event must be created first.</em></p>}
-                {!isEventPartnerLocationDataLoaded && props.eventId !== Guid.EMPTY && <p><em>Loading...</em></p>}
-                {isEventPartnerLocationDataLoaded && eventPartnerLocations.length === 0 && <p> <em>Sorry, there are no registered partners in your area.</em></p>}
-                {isEventPartnerLocationDataLoaded && eventPartnerLocations.length !== 0 && renderEventPartnerLocationsTable(eventPartnerLocations)}
-                {isPartnerLocationServicesDataLoaded && eventPartnerLocationServices && renderEventPartnerLocationServicesTable(eventPartnerLocationServices)}
+        <Container className='p-4 bg-white rounded my-5'>
+            <div className='d-flex align-items-center justify-content-between'>
+                <h4 className='fw-600 color-primary my-4'>Partners ({eventPartnerLocations?.length})</h4>
+                <div className='d-flex align-items-center' role="button">
+                    <Building aria-hidden="true" size={24} color="#96ba00" />
+                    <div className='p-18 color-primary ml-2'>Add partner</div>
+                </div>
             </div>
-        </>
+            {props.eventId === Guid.EMPTY && <p> <em>Event must be created first.</em></p>}
+            {!isEventPartnerLocationDataLoaded && props.eventId !== Guid.EMPTY && <p><em>Loading...</em></p>}
+            {isEventPartnerLocationDataLoaded && eventPartnerLocations.length === 0 && <p> <em>Sorry, there are no registered partners in your area.</em></p>}
+            {isEventPartnerLocationDataLoaded && eventPartnerLocations.length !== 0 && renderEventPartnerLocationsTable(eventPartnerLocations)}
+            {isPartnerLocationServicesDataLoaded && eventPartnerLocationServices && renderEventPartnerLocationServicesTable(eventPartnerLocationServices)}
+        </Container>
     );
 }
