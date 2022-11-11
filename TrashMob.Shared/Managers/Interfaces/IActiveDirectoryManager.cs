e649a022a24f5c469eddd9b2bbb86ce1b182696d
Joe Beernink <jobee@microsoft.com>
2022-11-11 08:26:42 -0800

    Reworking validation and adding tests

--- TrashMob.Shared/Managers/Interfaces/IActiveDirectoryManager.cs
+++ TrashMob.Shared/Managers/Interfaces/IActiveDirectoryManager.cs
@@ -8,6 +8,6 @@
     {
         Task<ActiveDirectoryResponseBase> CreateUserAsync(ActiveDirectoryNewUserRequest activeDirectoryNewUserRequest, CancellationToken cancellationToken = default);
 
-        Task<ActiveDirectoryResponseBase> ValidateUserAsync(ActiveDirectoryValidateNewUserRequest activeDirectoryNewUserRequest, CancellationToken cancellationToken = default);
+        Task<ActiveDirectoryResponseBase> ValidateNewUserAsync(ActiveDirectoryValidateNewUserRequest activeDirectoryNewUserRequest, CancellationToken cancellationToken = default);
     }
 }
