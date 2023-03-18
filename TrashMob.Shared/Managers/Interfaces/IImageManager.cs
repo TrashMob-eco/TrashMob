8277a176ef7c7c3fbad6a033867e8586ab2ab1af
Joe Beernink <jobee@microsoft.com>
2023-03-18 16:34:19 -0700

    Adding classes

---
+++ TrashMob.Shared/Managers/Interfaces/IImageManager.cs
@@ -0,0 +1,11 @@
+﻿namespace TrashMob.Shared.Managers.Interfaces
+{
+    using System.Threading.Tasks;
+    using TrashMob.Poco;
+
+    public interface IImageManager
+    {
+        public Task<bool> UpdloadImage(ImageUpload imageUpload);
+
+    }
+}
---
+++ TrashMob/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿namespace TrashMob.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using TrashMob.Poco;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        public ImageController() 
+        {
+        }
+
+        [HttpPost]
+        public Task<IActionResult> UploadImage([FromForm] ImageUpload imageUpload)
+        {
+            return null;
+        }
+    }
+}
