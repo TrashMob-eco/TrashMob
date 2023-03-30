b56ae5deadb896ca95397d32924b62a1a4df348e
Joe Beernink <jobee@microsoft.com>
2023-03-29 21:21:05 -0700

    Add security

--- TrashMob/Controllers/ImageController.cs
+++ TrashMob/Controllers/ImageController.cs
@@ -1,24 +1,36 @@
 ﻿namespace TrashMob.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Threading;
     using System.Threading.Tasks;
     using TrashMob.Poco;
+    using TrashMob.Security;
     using TrashMob.Shared.Managers.Interfaces;
 
     [Route("api/image")]
     [ApiController]
-    public class ImageController : ControllerBase
+    public class ImageController : SecureController
     {
         private readonly IImageManager imageManager;
+        private readonly IEventManager eventManager;
 
-        public ImageController(IImageManager imageManager) 
+        public ImageController(IImageManager imageManager, IEventManager eventManager) 
         {
             this.imageManager = imageManager;
         }
 
         [HttpPost]
-        public async Task<IActionResult> UploadImage([FromForm] ImageUpload imageUpload)
+        public async Task<IActionResult> UploadImage([FromForm] ImageUpload imageUpload, CancellationToken cancellationToken)
         {
+            var mobEvent = eventManager.GetAsync(imageUpload.ParentId, cancellationToken);
+            var authResult = await AuthorizationService.AuthorizeAsync(User, mobEvent, AuthorizationPolicyConstants.UserOwnsEntity);
+
+            if (!User.Identity.IsAuthenticated || !authResult.Succeeded)
+            {
+                return Forbid();
+            }
+
             await imageManager.UploadImage(imageUpload);
 
             return Ok();
