ad8f0a4216ea675dce33b3fdd6dd399f1dc6fa60
Joe Beernink <jobee@microsoft.com>
2023-04-09 13:47:37 -0700

    Adding PickupLocations Page

---
+++ TrashMobMobileApp/Features/Events/Components/PickupLocationList.razor.cs
@@ -0,0 +1,53 @@
+﻿namespace TrashMobMobileApp.Features.Events.Components
+{
+    using Microsoft.AspNetCore.Components;
+    using TrashMob.Models;
+    using TrashMobMobileApp.Data;
+    using TrashMobMobileApp.Features.Pickups;
+    
+    public partial class PickupLocationList
+    {
+        private List<PickupLocation> _pickupLocations = new();
+
+        [Parameter]
+        public string EventId
+        {
+            get
+            {
+                return eventId.ToString();
+            }
+            set
+            {
+                eventId = new Guid(value);
+            }
+        }
+
+        private Guid eventId;
+
+        private bool _isLoading;
+
+        [Inject]
+        public IMapRestService MapRestService { get; set; }
+
+        [Inject]
+        public IPickupLocationRestService PickupLocationRestService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ReInitializeAsync();
+        }
+
+        private async Task ReInitializeAsync()
+        {
+            _isLoading = true;
+            _pickupLocations = (await PickupLocationRestService.GetPickupLocationsAsync(eventId)).ToList();
+            _isLoading = false;
+        }
+
+        private async Task OnAddPickupLocationAsync()
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(new AddPickupLocation(MapRestService, PickupLocationRestService, eventId));
+            await ReInitializeAsync();
+        }
+    }
+}
---
+++ TrashMobMobileApp/Features/Events/Pages/PickupLocations.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace TrashMobMobileApp.Features.Events.Pages
+{
+    using Microsoft.AspNetCore.Components;
+    using TrashMobMobileApp.StateContainers;
+
+    public partial class PickupLocations
+    {
+        [Inject]
+        public UserStateInformation StateInformation { get; set; }
+
+        protected override void OnInitialized() => TitleContainer.Title = "Pickup Locations";
+    }
+}
