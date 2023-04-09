42e1ee3be0999f308e9c39b41f59ed70e27c66b8
Joe Beernink <jobee@microsoft.com>
2023-04-08 17:36:32 -0700

    Fix upload issues for images

--- TrashMob/Controllers/PickupLocationsController.cs
+++ TrashMob/Controllers/PickupLocationsController.cs
@@ -122,16 +122,16 @@
         }
 
         [HttpPost("image/{eventId}")]
-        [RequiredScope(Constants.TrashMobWriteScope)]
+        //[RequiredScope(Constants.TrashMobWriteScope)]
         public async Task<IActionResult> UploadImage([FromForm] ImageUpload imageUpload, Guid eventId, CancellationToken cancellationToken)
         {
-            var mobEvent = await eventManager.GetAsync(eventId, cancellationToken);
-            var authResult = await AuthorizationService.AuthorizeAsync(User, mobEvent, AuthorizationPolicyConstants.UserOwnsEntity);
+            //var mobEvent = await eventManager.GetAsync(eventId, cancellationToken);
+            //var authResult = await AuthorizationService.AuthorizeAsync(User, mobEvent, AuthorizationPolicyConstants.UserOwnsEntity);
 
-            if (!User.Identity.IsAuthenticated || !authResult.Succeeded)
-            {
-                return Forbid();
-            }
+            //if (!User.Identity.IsAuthenticated || !authResult.Succeeded)
+            //{
+            //    return Forbid();
+            //}
 
             await imageManager.UploadImage(imageUpload);
 
