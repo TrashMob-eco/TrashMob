c62851e06895dd704fea80848aeea0849a3af273
Joe Beernink <jobee@microsoft.com>
2023-04-12 19:54:03 -0700

    Adding manage partners

---
+++ TrashMobMobileApp/Features/Events/Components/CreateEventStepper/ManageEventPartners.razor.cs
@@ -0,0 +1,55 @@
+﻿namespace TrashMobMobileApp.Features.Events.Components
+{
+    using Microsoft.AspNetCore.Components;
+    using TrashMobMobileApp.Data;
+    using TrashMob.Models;
+    using TrashMobMobileApp.Extensions;
+    using TrashMob.Models.Poco;
+
+    public partial class ManageEventPartners
+    {
+        private bool _isLoading;
+        private bool _isCreated;
+
+        [Inject]
+        public IEventPartnerLocationServiceRestService EventPartnerLocationServiceRestService { get; set; }
+
+        [Parameter]
+        public Event Event { get; set; }
+
+        [Parameter]
+        public EventCallback OnStepFinished { get; set; }
+
+        private IEnumerable<DisplayEventPartnerLocation> displayEventPartnerLocations;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                TitleContainer.Title = "Manage Partners for Event";
+                _isLoading = true;
+                displayEventPartnerLocations = await EventPartnerLocationServiceRestService.GetEventPartnerLocationsAsync(Event.Id);
+                _isLoading = false;
+            }
+            catch (Exception ex)
+            {
+                if (ex.IsClosedStreamException())
+                {
+                    return;
+                }
+            }
+            finally
+            {
+                _isLoading = false;
+            }
+        }
+
+        private async Task OnStepFinishedAsync()
+        {
+            if (OnStepFinished.HasDelegate)
+            {
+                await OnStepFinished.InvokeAsync();
+            }
+        }
+    }
+}
