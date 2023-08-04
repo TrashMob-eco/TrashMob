d03d8f5988cf99db18b2e6f21832fda2fc0d27f8
Joe Beernink <jobee@microsoft.com>
2023-08-04 06:55:13 -0700

    first update

---
+++ TrashMob/Controllers/IFTTT/v1/UserController.cs
@@ -0,0 +1,19 @@
+﻿namespace TrashMob.Controllers.IFTTT
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using TrashMob.Security;
+
+    [Route("api/ifttt/v1/[controller]")]
+    [ApiController]
+    public class UserController : SecureController
+    {
+        [HttpGet]
+        [Authorize(Policy = AuthorizationPolicyConstants.ValidUser)]
+        public ActionResult Index()
+        {
+            var 
+            
+        }
+    }
+}
