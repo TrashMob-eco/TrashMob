3491f0909e1eadca9b2b71c4b962141687b14e3d
Liana Marulanda <lmarulanda@microsoft.com>
2023-08-15 15:06:27 -0700

    remove extra \n

--- TrashMob/client-app/src/components/EventManagement/ShareToSocialsModal.tsx
+++ TrashMob/client-app/src/components/EventManagement/ShareToSocialsModal.tsx
@@ -36,7 +36,7 @@ export const SocialsModal: React.FC<ModalProps> = (props) => {
 
   const getShareMsgContent = (provider: string) => {
     return `Join my next ${provider == 'twitter' ? "@TrashMobEco" : "TrashMob.eco"} event on ${eventDate} at ${eventTime} in ${props.createdEvent.city}.\n` +
-      `Sign up using the link for more details. Help me clean up ${props.createdEvent.city}!\n`
+      `Sign up using the link for more details. Help me clean up ${props.createdEvent.city}!`
   }
 
   const EventLink = () => {
