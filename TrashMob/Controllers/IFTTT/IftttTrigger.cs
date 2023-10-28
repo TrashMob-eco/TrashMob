72a1ca4afe14c6c7cfbd349513ccef944bf205b4
Joe Beernink <jobee@microsoft.com>
2023-10-28 10:24:32 -0700

    Adding test/Setup controller

---
+++ TrashMob/Controllers/IFTTT/IftttTrigger.cs
@@ -0,0 +1,15 @@
+﻿namespace TrashMob.Controllers.IFTTT
+{
+    using System.Collections.Generic;
+
+    public class IftttTrigger
+    {
+        public Dictionary<string, string> new_event_created { get; set; } = new Dictionary<string, string>()
+        {
+            { "city", "Issaquah" },
+            { "region", "Washington" },
+            { "country", "United States" },
+            { "postalCode", "98029" }
+        };
+    }
+}
---
+++ TrashMob/Controllers/IFTTT/v1/SetupController.cs
@@ -0,0 +1,31 @@
+﻿namespace TrashMob.Controllers.IFTTT
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using TrashMob.Security;
+    
+    [Route("api/ifttt/v1/test/[controller]")]
+    [ApiController]
+
+    public class SetupController : SecureController
+    {
+        [HttpGet]
+        [Authorize(Policy = AuthorizationPolicyConstants.ValidUser)]
+        [Authorize(Policy = AuthorizationPolicyConstants.IftttServiceKey)]
+        public ActionResult GetInfo()
+        {
+            var dataResponse = new DataResponse();
+
+            var accessToken = HttpContext.Request.Headers.Authorization;
+
+            var sampleResponse = new SampleResponse
+            {
+                accessToken = accessToken,
+            };
+
+            dataResponse.Data = sampleResponse;
+
+            return Ok(dataResponse);
+        }
+    }
+}
