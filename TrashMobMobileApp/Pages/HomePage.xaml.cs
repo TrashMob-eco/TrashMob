41555ec2247e178fdf084c51930863440e768ed0
Joe Beernink <jobee@microsoft.com>
2023-11-23 13:40:38 -0800

    Adding some buttons. Updating nugets to latests

--- TrashMobMobileApp/Pages/HomePage.xaml.cs
+++ TrashMobMobileApp/Pages/HomePage.xaml.cs
@@ -10,4 +10,43 @@ public partial class HomePage : ContentPage
         _viewModel = viewModel;
         BindingContext = _viewModel;
     }
+
+    private void ContactUs_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ContactUsPage(new ContactUsViewModel()));
+    }
+
+    private void MyDashboard_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MyDashboardPage(new MyDashboardViewModel()));
+    }
+
+    private void SearchEvents_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SearchEventsPage(new SearchEventsViewModel()));
+    }
+
+    private void CreateEvent_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CreateEventPage(new CreateEventViewModel()));
+    }
+
+    private void SubmitLitterReport_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SubmitLitterReportPage(new SubmitLitterReportViewModel()));
+    }
+
+    private void SearchLitterReports_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SearchLitterReportsPage(new SearchLitterReportsViewModel()));
+    }
+
+    private void SetMyLocationPreference_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SetUserLocationPreferencePage(new UserLocationPreferenceViewModel()));
+    }
+
+    private void Logout_Clicked(object sender, EventArgs e)
+    {
+    }
 }
