478eafa8861cf64a4ec8445a8a2269d85e009865
Joe Beernink <jobee@microsoft.com>
2023-12-03 13:36:52 -0800

    A few more adjustments

--- TrashMobMobileApp/Pages/ContactUsPage.xaml.cs
+++ TrashMobMobileApp/Pages/ContactUsPage.xaml.cs
@@ -9,5 +9,6 @@ public partial class ContactUsPage : ContentPage
 		InitializeComponent();
         _viewModel = viewModel;
         BindingContext = _viewModel;
+
     }
 }
---
+++ TrashMobMobileApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+namespace TrashMobMobileApp.Pages;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel _viewModel;
+
+    public MainPage(MainViewModel viewModel)
+	{
+		InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    private void ContactUs_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ContactUsPage(new ContactUsViewModel()));
+    }
+
+    private void MyDashboard_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MyDashboardPage(new MyDashboardViewModel()));
+    }
+
+    private void SearchEvents_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SearchEventsPage(new SearchEventsViewModel()));
+    }
+
+    private void CreateEvent_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CreateEventPage(new CreateEventViewModel()));
+    }
+
+    private void SubmitLitterReport_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SubmitLitterReportPage(new SubmitLitterReportViewModel()));
+    }
+
+    private void SearchLitterReports_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SearchLitterReportsPage(new SearchLitterReportsViewModel()));
+    }
+
+    private void SetMyLocationPreference_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SetUserLocationPreferencePage(new UserLocationPreferenceViewModel()));
+    }
+
+    private void Logout_Clicked(object sender, EventArgs e)
+    {
+    }
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+        await _viewModel.Init();
+    }
+}
