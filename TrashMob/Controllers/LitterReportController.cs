0ca5283c9e967985c4e2618972798d766a24e21c
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-04-21 12:17:16 -0700

    Fix url (#1340)
    * Adding Litter Report functionality
    
    * Adding litter reports basics
    
    * Reworking Litter Reports
    
    * Consolidating create litter report pages
    
    * Updating litterreports
    
    * Updating litter report to not have nulls
    
    * Revert gitversion to 5000
    
    * Update gitversion
    
    * Add log statement for litter
    
    * Remove extra }
    
    * Improving image upload
    
    * Add new method for uploading Litter Reports
    
    * Updated app
    
    * Fixing uploads
    
    * Fix issue with override of Id
    
    * Fix auth issues
    
    * Fix return type on Litter reports
    
    * Adding get litter method
    
    * Adding get of litter report
    
    * Fix merge issue
    
    ---------
    
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

--- TrashMob/Controllers/LitterReportController.cs
+++ TrashMob/Controllers/LitterReportController.cs
@@ -39,13 +39,12 @@ namespace TrashMob.Controllers
             this.logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet("{litterReportId}")]
         public async Task<IActionResult> GetLitterReport(Guid litterReportId, CancellationToken cancellationToken)
         {
             var result = await litterReportManager.GetAsync(litterReportId, cancellationToken).ConfigureAwait(false);
             return Ok(result);
         }
-
 
         [HttpGet]
         public async Task<IActionResult> GetLitterReports(CancellationToken cancellationToken)
