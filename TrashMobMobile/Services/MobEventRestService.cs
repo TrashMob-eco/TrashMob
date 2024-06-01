85464c0f3ab75d04a26a12a241fbac093bae444c
Markus Luszczymak <marmeladenmuffin@live.de>
2024-06-01 18:03:18 +0200

    feat GetLocationsByTimeRangeAsync() converts the Time to EST

--- TrashMobMobile/Services/MobEventRestService.cs
+++ TrashMobMobile/Services/MobEventRestService.cs
@@ -1,6 +1,7 @@
 ﻿namespace TrashMobMobile.Data;
 
 using System.Diagnostics;
+using System.Globalization;
 using System.Net.Http.Json;
 using Newtonsoft.Json;
 using TrashMob.Models;
@@ -198,7 +199,10 @@ public class MobEventRestService : RestServiceBase, IMobEventRestService
     public async Task<IEnumerable<Location>> GetLocationsByTimeRangeAsync(DateTimeOffset startDate,
         DateTimeOffset endDate, CancellationToken cancellationToken = default)
     {
-        var requestUri = Controller + "/locationsbytimerange?startTime=" + startDate + "&endTime=" + endDate;
+        var convertedStartTime = startDate.ToString(@"yyyy/MM/dd hh:mm:ss tt", new CultureInfo("en-US"));
+        var convertedEndTime = endDate.ToString(@"yyyy/MM/dd hh:mm:ss tt", new CultureInfo("en-US"));
+
+        var requestUri = Controller + "/locationsbytimerange?startTime=" + convertedStartTime + "&endTime=" + convertedEndTime;
 
         using (var response = await AnonymousHttpClient.GetAsync(requestUri, cancellationToken))
         {
