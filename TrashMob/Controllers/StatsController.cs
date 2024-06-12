dc747b7dc44f1aeeb708cc51de0d4311d45e813d
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-06-11 20:40:55 -0700

    Add Cors to the Endpoint Directly (#1522)
    * Trying CORS fix
    Adding renovate.json
    
    * Try adding EnableCors directly to the Controller
    
    ---------
    
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

--- TrashMob/Controllers/StatsController.cs
+++ TrashMob/Controllers/StatsController.cs
@@ -3,25 +3,14 @@
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Cors;
     using Microsoft.AspNetCore.Mvc;
     using TrashMob.Shared.Managers.Interfaces;
 
+    [EnableCors("_myAllowSpecificOrigins")]
     [Route("api/stats")]
-    public class StatsController : BaseController
+    public class StatsController(IEventSummaryManager eventSummaryManager) : BaseController
     {
-        private readonly IEventAttendeeManager eventAttendeeManager;
-        private readonly IEventManager eventManager;
-        private readonly IEventSummaryManager eventSummaryManager;
-
-        public StatsController(IEventManager eventManager,
-            IEventSummaryManager eventSummaryManager,
-            IEventAttendeeManager eventAttendeeManager)
-        {
-            this.eventManager = eventManager;
-            this.eventSummaryManager = eventSummaryManager;
-            this.eventAttendeeManager = eventAttendeeManager;
-        }
-
         [HttpGet]
         public async Task<IActionResult> GetStats(CancellationToken cancellationToken)
         {
