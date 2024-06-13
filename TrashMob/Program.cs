4ea3229e0eeaa4756155f517e4945e11e7eb3dab
Joe Beernink <jobee@microsoft.com>
2024-06-13 08:01:20 -0700

    Added HttpsRedirects back in

--- TrashMob/Program.cs
+++ TrashMob/Program.cs
@@ -224,8 +224,8 @@ namespace TrashMob
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-             
-            // app.UseHttpsRedirection();
+
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
