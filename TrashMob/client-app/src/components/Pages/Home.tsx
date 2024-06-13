7ed9e37a3d14a755366d84b5800453aeaa3404e4
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-06-12 22:16:58 -0700

    Remove https redirections (#1544)
    * More CORS attempts
    
    * Try more localhost options
    
    * Try adding UseForwardedHeaders
    
    * Remove HttpsRedirection
    
    ---------
    
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

--- TrashMob/client-app/src/components/Pages/Home.tsx
+++ TrashMob/client-app/src/components/Pages/Home.tsx
@@ -36,7 +36,7 @@ const Home: FC<HomeProps> = ({ isUserLoaded, currentUser, history, location, mat
 
         const headers = getDefaultHeaders('GET');
 
-        fetch('https://as-tm-dev-westus2.azurewebsites.net/api/stats', {
+        fetch('/api/stats', {
             method: 'GET',
             headers: headers
         })
