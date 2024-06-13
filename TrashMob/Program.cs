35e286c5b56595126fab023a9110e7362bae6e17
Joe Beernink <jobee@microsoft.com>
2024-06-12 19:36:43 -0700

    Try more localhost options

--- TrashMob/Program.cs
+++ TrashMob/Program.cs
@@ -111,7 +111,7 @@ namespace TrashMob
                 options.AddPolicy(MyAllowSpecificOrigins,
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+                        policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:3000/", "https://localhost:3000/")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
                             .AllowCredentials();
@@ -209,7 +209,7 @@ namespace TrashMob
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+             
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
