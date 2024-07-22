0e9f4fa264d86e67529aa1872910f039cd81aa80
Joe Beernink <jobee@microsoft.com>
2024-07-21 18:03:09 -0700

    Merged from Main

--- TrashMobMobile/App.xaml.cs
+++ TrashMobMobile/App.xaml.cs
@@ -6,11 +6,6 @@ public partial class App : Application
 {
     public App()
     {
-        // TODO: uncomment if we need syncfusion
-        //var sfLicense = Constants.GetSyncfusionKey();
-
-        //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(sfLicense);
-
         InitializeComponent();
 
         MainPage = new AppShell();
