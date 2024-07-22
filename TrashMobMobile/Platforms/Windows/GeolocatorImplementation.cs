83069cbcdafcd6c93cbee8ef7f27f2b20726fa49
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-07-21 21:43:41 -0700

    Fixing missing class for Windows build (#1582)
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

---
+++ TrashMobMobile/Platforms/Windows/GeolocatorImplementation.cs
@@ -0,0 +1,15 @@
+﻿namespace TrashMobMobile
+{
+    using System;
+    using System.Threading.Tasks;
+    using TrashMobMobile.Services;
+
+    public class GeolocatorImplementation : IGeolocator
+    {
+        // Do not need to implement this method for Windows
+        public Task StartListening(IProgress<Location> positionChangedProgress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
