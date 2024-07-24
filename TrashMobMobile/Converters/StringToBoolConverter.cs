0d65ee3e71aba2ea1d0d6d140a95200903f24e00
vtserej <vtserej@gmail.com>
2024-07-24 00:48:55 -0400

    first version (#1586)

---
+++ TrashMobMobile/Converters/StringToBoolConverter.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+
+namespace TrashMobMobile.Converters;
+
+public class StringToBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return !string.IsNullOrEmpty(value as string);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
