a31bfd710dadceeeb2632f2abc12821053a7fd7b
Dopp <dopp@ddxasia.com>
2024-09-18 20:19:47 +0700

    refactor(react-query): Remove unnecessary state from Homepage

--- TrashMob/client-app/src/services/stats.ts
+++ TrashMob/client-app/src/services/stats.ts
@@ -2,7 +2,13 @@ import { ApiService } from ".";
 import StatsData from "../components/Models/StatsData";
 
 export type GetStats_Response = StatsData;
-export const GetStats = () => ({ key: ['/stats'], service: async () => ApiService('public').fetchData<GetStats_Response>({ url: `/stats`, method: 'get' }) });
+export const GetStats = () => ({
+  key: ['/stats'],
+  service: async () => 
+    ApiService('public')
+      .fetchData<GetStats_Response>({ url: `/stats`, method: 'get' })
+      .then((res) => res.data)
+});
 
 export type GetStatsForUser_Params = { userId: string; };
 export type GetStatsForUser_Response = StatsData;
