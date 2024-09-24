1cc03e4fa7bc5c4dc0d1fa4e8af3ce3cc08eeeee
Pitipat Srichairat <pitipat.dop@gmail.com>
2024-09-24 10:13:03 +0700

    Extract AzureSearchLocationInput into separate component (#1634)
    * extract searchlocationInput into component
    
    * replace AsyncTypeAhead with new component
    
    * remove paginate props
    
    * remove azure-map-control from AzureSearchLocationInput
    
    ---------
    
    Co-authored-by: Dopp <dopp@ddxasia.com>

--- TrashMob/client-app/src/services/maps.ts
+++ TrashMob/client-app/src/services/maps.ts
@@ -2,7 +2,7 @@
 // maps
 
 import axios from "axios";
-import SearchAddressData from "../components/Models/SearchAddressData";
+import { AzureMapSearchAddressResult } from "../components/Models/AzureMapSearchAddress";
 import AddressData from "../components/Models/AddressData";
 import { ApiService } from ".";
 
@@ -10,8 +10,11 @@ export type GetMaps_Response = string;
 export const GetMaps = () => ({ key: ['/maps'], service: async () => ApiService('public').fetchData<GetMaps_Response>({ url: `/maps`, method: 'get' }) });
 
 export type AzureMapSearchAddress_Params = { azureKey: string; query: string; }
-export type AzureMapSearchAddress_Response = SearchAddressData;
-export const AzureMapSearchAddress = () => ({ key: ['AzureMapSearchAddress'], service: async (params: AzureMapSearchAddress_Params) => axios.get<AzureMapSearchAddress_Response>(`https://atlas.microsoft.com/search/address/json?typeahead=true&subscription-key=${params.azureKey}&api-version=1.0&query=${params.query}`) });
+export type AzureMapSearchAddress_Response = AzureMapSearchAddressResult;
+export const AzureMapSearchAddress = () => ({
+  key: (query: string) => ['AzureMapSearchAddress', query],
+  service: async (params: AzureMapSearchAddress_Params) => axios.get<AzureMapSearchAddress_Response>(`https://atlas.microsoft.com/search/address/json?typeahead=true&subscription-key=${params.azureKey}&api-version=1.0&query=${params.query}`)
+});
 
 export type AzureMapSearchAddressReverse_Params = { azureKey: string; lat: number; long: number; }
 export type AzureMapSearchAddressReverse_Response = AddressData;
