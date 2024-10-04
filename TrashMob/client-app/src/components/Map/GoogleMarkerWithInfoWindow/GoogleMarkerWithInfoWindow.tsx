972b3a18aec76b6a4d5704095249e558497b5e21
Dopp <dopp@ddxasia.com>
2024-10-05 00:08:53 +0700

    fix issue that marker mouseover doesnot trigger infowindow to appear

--- TrashMob/client-app/src/components/Map/GoogleMarkerWithInfoWindow/GoogleMarkerWithInfoWindow.tsx
+++ TrashMob/client-app/src/components/Map/GoogleMarkerWithInfoWindow/GoogleMarkerWithInfoWindow.tsx
@@ -17,15 +17,14 @@ export const GoogleMarkerWithInfoWindow = (props: MarkerWithInfoWindowProps) => 
     const { infoWindowTrigger, infoWindowProps, infoWindowContent, ...markerProps } = props;
     const [markerRef, marker] = useAdvancedMarkerRef();
     const [infoWindowShown, setInfoWindowShown] = useState<boolean>(false);
-
     const triggerProps =
         infoWindowTrigger === 'click'
             ? {
                   onClick: () => setInfoWindowShown(!infoWindowShown),
               }
             : {
-                  onMouseOver: () => setInfoWindowShown(true),
-                  onMouseOut: () => setInfoWindowShown(false),
+                  onMouseEnter: () => setInfoWindowShown(true),
+                  onMouseLeave: () => setInfoWindowShown(false),
               };
 
     return (
