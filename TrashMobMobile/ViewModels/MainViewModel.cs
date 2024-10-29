a6b923795625708ba64d15375481b1d203fe5bb1
Joe Beernink <jobee@microsoft.com>
2024-10-28 21:06:36 -0700

    Adding Litter Images to Maps

--- TrashMobMobile/ViewModels/MainViewModel.cs
+++ TrashMobMobile/ViewModels/MainViewModel.cs
@@ -3,6 +3,8 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using TrashMob.Models;
+using TrashMob.Models.Poco;
 using TrashMobMobile.Authentication;
 using TrashMobMobile.Config;
 using TrashMobMobile.Extensions;
@@ -12,10 +14,12 @@ public partial class MainViewModel(IAuthService authService,
     IUserRestService userRestService,
     IStatsRestService statsRestService,
     IMobEventManager mobEventManager,
+    ILitterReportManager litterReportManager,
     INotificationService notificationService) : BaseViewModel(notificationService)
 {
     private readonly IAuthService authService = authService;
     private readonly IMobEventManager mobEventManager = mobEventManager;
+    private readonly ILitterReportManager litterReportManager = litterReportManager;
     private readonly IStatsRestService statsRestService = statsRestService;
     private readonly IUserRestService userRestService = userRestService;
     private readonly INotificationService notificationService = notificationService;
@@ -37,6 +41,10 @@ public partial class MainViewModel(IAuthService authService,
     private string? welcomeMessage;
 
     public ObservableCollection<EventViewModel> UpcomingEvents { get; set; } = [];
+    
+    public ObservableCollection<LitterReportViewModel> LitterReports { get; set; } = [];
+
+    public ObservableCollection<AddressViewModel> Addresses { get; set; } = [];
 
     public EventViewModel SelectedEvent
     {
@@ -89,7 +97,10 @@ public partial class MainViewModel(IAuthService authService,
 
                 UserLocationDisplay = $"{UserLocation.City}, {UserLocation.Region}";
 
+                Addresses.Clear();
+
                 await RefreshEvents();
+                await RefreshLitterReports();
 
                 IsBusy = false;
             }
@@ -118,6 +129,29 @@ public partial class MainViewModel(IAuthService authService,
         StatisticsViewModel.TotalHours = stats.TotalHours;
     }
 
+    private async Task RefreshLitterReports()
+    {
+        LitterReports.Clear();
+        var litterFilter = new LitterReportFilter
+        {
+            LitterReportStatusId = (int)LitterReportStatusEnum.New,
+            IncludeLitterImages = true,
+        };
+
+        var litterReports = await litterReportManager.GetLitterReportsAsync(litterFilter);
+
+        foreach (var litterReport in litterReports.OrderBy(l => l.CreatedDate))
+        {
+            var vm = litterReport.ToLitterReportViewModel(notificationService);
+            LitterReports.Add(vm);
+
+            foreach (var litterImageViewModel in vm.LitterImageViewModels)
+            {
+                Addresses.Add(litterImageViewModel.Address);
+            }
+        }
+    }
+
     private async Task RefreshEvents()
     {
         UpcomingEvents.Clear();
@@ -132,6 +166,7 @@ public partial class MainViewModel(IAuthService authService,
             vm.IsUserAttending = eventsUserIsAttending.Any(e => e.Id == mobEvent.Id);
 
             UpcomingEvents.Add(vm);
+            Addresses.Add(vm.Address);
         }
     }
 
@@ -178,7 +213,7 @@ public partial class MainViewModel(IAuthService authService,
             City = Settings.DefaultCity,
             Region = Settings.DefaultRegion,
             Country = Settings.DefaultCountry,
-            Location = new Location(Settings.DefaultLatitude, Settings.DefaultLongitude),
+            Location = new Microsoft.Maui.Devices.Sensors.Location(Settings.DefaultLatitude, Settings.DefaultLongitude),
         };
     }
 }
