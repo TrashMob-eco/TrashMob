0f6670c81b8cfeaf4caa4464621538cb1cfc789c
vtserej <vtserej@gmail.com>
2024-10-29 23:45:28 -0400

    First Version

---
+++ TrashMobMobile/Controls/CustomPin.cs
@@ -0,0 +1,15 @@
+using Microsoft.Maui.Controls.Maps;
+
+namespace TrashMobMobile.Controls;
+
+public class CustomPin : Pin
+{
+    public static readonly BindableProperty ImageSourceProperty =
+        BindableProperty.Create(nameof(ImageSource), typeof(ImageSource), typeof(CustomPin));
+
+    public ImageSource? ImageSource
+    {
+        get => (ImageSource?)GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+}
