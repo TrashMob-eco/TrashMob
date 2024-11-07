9c61f20082e6ee2292eea86413e98e47b118f4d2
Dany Pellerin <pellerin.dany@gmail.com>
2024-11-06 21:57:48 -0500

    Implement github actions to track failing linting and formatting (#1776)
    Format code
    
    improve action

--- TrashMob/client-app/vite.config.ts
+++ TrashMob/client-app/vite.config.ts
@@ -1,12 +1,12 @@
-import react from "@vitejs/plugin-react"
-import path from "path"
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
     return {
         resolve: {
             alias: {
-                "@": path.resolve(__dirname, "./src"),
+                '@': path.resolve(__dirname, './src'),
             },
         },
         build: {
