ec1b1e0b10ea39bbd2fc6089b5b2669db143184b
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-12-01 14:25:50 -0800

    Add Logging for Litter Report Status (#1804)
    * Improving Add and Delete of EventLitterReports to track status
    
    * Try a different approach to litter report status change
    
    * Fix
    
    * Adding some logging to see what is going on to LitterReport Status
    
    ---------
    
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

--- TrashMob.Shared/Managers/Events/EventLitterReportManager.cs
+++ TrashMob.Shared/Managers/Events/EventLitterReportManager.cs
@@ -6,6 +6,7 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
     using TrashMob.Models;
     using TrashMob.Shared.Managers.Interfaces;
     using TrashMob.Shared.Persistence.Interfaces;
@@ -13,15 +14,17 @@
     public class EventLitterReportManager : BaseManager<EventLitterReport>, IBaseManager<EventLitterReport>, IEventLitterReportManager
     {
         private readonly IEmailManager emailManager;
+        private readonly ILogger<EventLitterReportManager> logger;
         private readonly IKeyedRepository<Event> eventRepository;
         private readonly IKeyedRepository<LitterReport> litterReportRepository;
 
         public EventLitterReportManager(IBaseRepository<EventLitterReport> repository, IKeyedRepository<Event> eventRepository, IKeyedRepository<LitterReport> litterReportRepository,
-            IEmailManager emailManager) : base(repository)
+            IEmailManager emailManager, ILogger<EventLitterReportManager> logger) : base(repository)
         {
             this.eventRepository = eventRepository;
             this.litterReportRepository = litterReportRepository;
             this.emailManager = emailManager;
+            this.logger = logger;
         }
 
         public override async Task<IEnumerable<EventLitterReport>> GetByParentIdAsync(Guid parentId,
@@ -35,21 +38,25 @@
 
         public override async Task<EventLitterReport> AddAsync(EventLitterReport eventLitterReport, CancellationToken cancellationToken)
         {
+            logger.LogInformation($"Adding EventLitterReport for EventId {eventLitterReport.EventId} and LitterReportId {eventLitterReport.LitterReportId}");
             var litterReport = litterReportRepository.Get(l => l.Id == eventLitterReport.LitterReportId).FirstOrDefault();
             litterReport.LitterReportStatusId = (int)LitterReportStatusEnum.Assigned;
             await litterReportRepository.UpdateAsync(litterReport);
+            logger.LogInformation("Updated LitterReport Status for LitterReportId {LitterReportId} to {LitterReportStatus}", eventLitterReport.LitterReportId, (int)LitterReportStatusEnum.Assigned);
 
             return await Repository.AddAsync(eventLitterReport);
         }
 
         public override async Task<int> Delete(Guid parentId, Guid secondId, CancellationToken cancellationToken)
         {
+            logger.LogInformation($"Deleting EventLitterReport for EventId {parentId} and LitterReportId {secondId}");
             var eventLitterReport = await Repository.Get(ea => ea.EventId == parentId && ea.LitterReportId == secondId)
                 .FirstOrDefaultAsync(cancellationToken);
 
             var litterReport = litterReportRepository.Get(l => l.Id == eventLitterReport.LitterReportId).FirstOrDefault();
             litterReport.LitterReportStatusId = (int)LitterReportStatusEnum.New;
             await litterReportRepository.UpdateAsync(litterReport);
+            logger.LogInformation("Updated LitterReport Status for LitterReportId {LitterReportId} to {LitterReportStatus}", eventLitterReport.LitterReportId, (int)LitterReportStatusEnum.New);
 
             return await Repository.DeleteAsync(eventLitterReport);
         }
