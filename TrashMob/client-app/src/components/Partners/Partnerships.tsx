b21aa7e0cc5dd371e928d4c5a80b18f196e22b74
Pitipat Srichairat <pitipat.dop@gmail.com>
2024-12-15 08:16:18 +0700

    refactor(partnerships): convert partnerships page to tailwind (#1855)
    Co-authored-by: Dopp <dopp@ddxasia.com>

--- TrashMob/client-app/src/components/Partners/Partnerships.tsx
+++ TrashMob/client-app/src/components/Partners/Partnerships.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Ihca from '../assets/partnerships/logos/IHCA.png';
 import TroutUnlimited from '../assets/partnerships/logos/TROUTUNLIMITEDLogo.png';
@@ -10,6 +9,7 @@ import Dollarsign from '../assets/partnerships/dollarsign.svg';
 import Garbage from '../assets/partnerships/garbage.png';
 
 import { HeroSection } from '../Customization/HeroSection';
+import { Button } from '../ui/button';
 
 export const Partnerships: FC<any> = () => {
     React.useEffect(() => {
@@ -17,122 +17,137 @@ export const Partnerships: FC<any> = () => {
     });
 
     return (
-        <>
+        <div className='tailwind'>
             <HeroSection Title='Partnerships' Description='Connecting you with volunteers.' />
-            <Container className='py-5'>
-                <Row>
-                    <Col sm={7}>
-                        <h1 className='fw-600'>What are partnerships?</h1>
-                        <h4>
-                            Partnering with local cities and businesses can connect TrashMob event volunteers with the
-                            supplies and services they need.
-                        </h4>
-                        <p>
-                            Partners can include cities, local businesses, and branches/locations of larger companies.
-                            Services can include trash hauling and disposal locations, and supplies can include buckets,
-                            grabber tools, and safety equipment. Looking for supplies and services for your next event?
-                            Invite a partnership from your city! Have supplies and services to offer? Become a partner!
-                        </p>
-                    </Col>
-                    <Col sm={5}>
-                        <Row className='pl-3'>
-                            <h1 className='fw-600'>Our Partners</h1>
-                        </Row>
-                        <Row className='pl-3'>
-                            <a href='https://issaquahhighlands.com/' target='_blank' rel='noreferrer' className='m-2'>
-                                <Image
-                                    src={Ihca}
-                                    alt='Issaquah Highlands Community Association'
-                                    className='graphic-large mx-auto'
-                                />
-                            </a>
-                            <a href='https://troutunlimited.org' target='_blank' rel='noreferrer' className='m-2'>
-                                <Image src={TroutUnlimited} alt='Trout Unlimited' className='graphic-large mx-auto' />
-                            </a>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-            <div className='w-100 bg-white'>
-                <Container className='py-5 px-0'>
-                    <Row className='max-width-container mx-auto align-items-center'>
-                        <Col sm={6}>
-                            <div className='d-flex flex-column align-items-start'>
-                                <p className='font-size-h4'>
+            <section>
+                <div className='container !py-5'>
+                    <div className='grid grid-cols-12 gap-4'>
+                        <div className='col-span-12 sm:col-span-7'>
+                            <h1 className='font-semibold'>What are partnerships?</h1>
+                            <h4>
+                                Partnering with local cities and businesses can connect TrashMob event volunteers with
+                                the supplies and services they need.
+                            </h4>
+                            <p>
+                                Partners can include cities, local businesses, and branches/locations of larger
+                                companies. Services can include trash hauling and disposal locations, and supplies can
+                                include buckets, grabber tools, and safety equipment. Looking for supplies and services
+                                for your next event? Invite a partnership from your city! Have supplies and services to
+                                offer? Become a partner!
+                            </p>
+                        </div>
+                        <div className='col-span-12 sm:col-span-5 text-center sm:text-left'>
+                            <h1 className='font-semibold'>Our Partners</h1>
+                            <div className='flex flex-row justify-center sm:justify-start'>
+                                <a
+                                    href='https://issaquahhighlands.com/'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className='m-2'
+                                >
+                                    <img
+                                        src={Ihca}
+                                        alt='Issaquah Highlands Community Association'
+                                        className='h-20 mx-auto'
+                                    />
+                                </a>
+                                <a href='https://troutunlimited.org' target='_blank' rel='noreferrer' className='m-2'>
+                                    <img src={TroutUnlimited} alt='Trout Unlimited' className='h-20 mx-auto' />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='bg-white'>
+                <div className='container py-5'>
+                    <div className='grid grid-cols-12 gap-x-4 gap-y-8'>
+                        <div className='col-span-12 sm:col-span-6'>
+                            <div className='flex flex-column items-center text-center sm:items-start sm:!text-left'>
+                                <p className='text-2xl'>
                                     No partner for your event? Invite local government or business to join TrashMob.eco
                                     as a partner!
                                 </p>
-                                <Link className='btn btn-primary banner-button' to='/inviteapartner'>
-                                    Invite a partner
-                                </Link>
+                                <Button asChild>
+                                    <Link to='/inviteapartner'>Invite a partner</Link>
+                                </Button>
                             </div>
-                        </Col>
-                        <Col sm={6}>
-                            <div className='d-flex flex-column align-items-start'>
-                                <p className='font-size-h4'>
+                        </div>
+                        <div className='col-span-12 sm:col-span-6'>
+                            <div className='flex flex-column items-center text-center sm:items-start sm:!text-left'>
+                                <p className='text-2xl'>
                                     Have supplies and services to offer? Submit an application to become a TrashMob.eco
                                     partner!
                                 </p>
-                                <Link className='btn btn-primary banner-button' to='/becomeapartner'>
-                                    Become a partner
-                                </Link>
+                                <Button asChild>
+                                    <Link to='/becomeapartner'>Become a partner</Link>
+                                </Button>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            <Container fluid className='text-center py-5 px-0'>
-                <h2 className='font-weight-bold mb-3'>Partnerships support the volunteers</h2>
-                <span>Services and supplies offered can include:</span>
-                <Row className='justify-content-center'>
-                    <Row xs={1} sm={2} md={4} className='w-50 mt-5 mx-auto justify-content-around    '>
-                        <Col className='d-flex flex-column align-items-center mb-5 mb-md-0'>
-                            <Image src={Safetykits} alt='Safety kits' className='graphic-large mt-0' />
-                            <span className='font-weight-bold mt-2'>Safety gear and roadside signs</span>
-                        </Col>
-                        <Col className='d-flex flex-column align-items-center mb-5 mb-md-0'>
-                            <Image src={Supplies} alt='Supplies' className='graphic-large mt-0' />
-                            <span className='font-weight-bold mt-2'>Pickup supplies such as garbage bags</span>
-                        </Col>
-                        <Col className='d-flex flex-column align-items-center mb-5 mb-md-0'>
-                            <Image src={TrashDisposal} alt='Trash Disposal & Hauling' className='graphic-large mt-0' />
-                            <span className='font-weight-bold mt-2'>
-                                Use of existing dumpsters and hauling of trash to disposal site
-                            </span>
-                        </Col>
-                        <Col className='d-flex flex-column align-items-center mb-5 mb-md-0'>
-                            <Image src={Dollarsign} alt='Dollar sign' className='graphic-large mt-0' />
-                            <span className='font-weight-bold mt-2'>
-                                <a href='https://www.trashmob.eco/donate'>Donations</a> to TrashMob.eco fund development
-                                of our platform and programs
-                            </span>
-                        </Col>
-                    </Row>
-                </Row>
-            </Container>
-
-            <Container fluid className='bg-white py-5'>
-                <Row className='container mx-auto px-0'>
-                    <Col sm={7}>
-                        <h1 className='fw-600'>Making the most out of partnerships</h1>
-                        <p className='para'>
-                            A successful clean up event depends upon a team of volunteers and the support of partners:
-                            community businesses, organizations and governments. Volunteer organizers set an event
-                            location, rally member support and utilize partnership provisions.
-                        </p>
-                        <p className='para'>
-                            TrashMob administrators confirm, approve and connect partners with event organizers.
-                            Partners and their form of support are indicated on event registration pages. Then local
-                            teamwork commences! Event organizers and partners coordinate access of supplies, services
-                            and instructions. Partners are selected upon availability and proximity to the event. Note:
-                            Supplied services from a given partner may vary by location/branch.{' '}
-                        </p>
-                    </Col>
-                    <Col sm={5} className='d-flex'>
-                        <Image src={Garbage} alt='garbage bags being picked up' className='mh-100 mt-0 m-auto' />
-                    </Col>
-                </Row>
-            </Container>
-        </>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className='text-center py-5 px-0'>
+                    <h2 className='font-weight-bold mb-3'>Partnerships support the volunteers</h2>
+                    <span>Services and supplies offered can include:</span>
+                    <div className='w-1/2 mx-auto grid grid-cols-4 !mt-12'>
+                        <div className='col-span-4 sm:col-span-2 md:col-span-1'>
+                            <div className='flex flex-col items-center !m-8'>
+                                <img src={Safetykits} alt='Safety kits' className='mt-0' />
+                                <span className='font-weight-bold mt-2'>Safety gear and roadside signs</span>
+                            </div>
+                        </div>
+                        <div className='col-span-4 sm:col-span-2 md:col-span-1'>
+                            <div className='flex flex-col items-center !m-8'>
+                                <img src={Supplies} alt='Supplies' className='mt-0' />
+                                <span className='font-weight-bold mt-2'>Pickup supplies such as garbage bags</span>
+                            </div>
+                        </div>
+                        <div className='col-span-4 sm:col-span-2 md:col-span-1'>
+                            <div className='flex flex-col items-center !m-8'>
+                                <img src={TrashDisposal} alt='Trash Disposal & Hauling' className='mt-0' />
+                                <span className='font-weight-bold mt-2'>
+                                    Use of existing dumpsters and hauling of trash to disposal site
+                                </span>
+                            </div>
+                        </div>
+                        <div className='col-span-4 sm:col-span-2 md:col-span-1'>
+                            <div className='flex flex-col items-center !m-8'>
+                                <img src={Dollarsign} alt='Dollar sign' className='mt-0' />
+                                <span className='font-weight-bold mt-2'>
+                                    <a href='https://www.trashmob.eco/donate'>Donations</a> to TrashMob.eco fund
+                                    development of our platform and programs
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='bg-card'>
+                <div className='container py-5'>
+                    <div className='grid grid-cols-12 gap-8'>
+                        <div className='col-span-12 sm:col-span-7'>
+                            <h1 className='font-semibold'>Making the most out of partnerships</h1>
+                            <p>
+                                A successful clean up event depends upon a team of volunteers and the support of
+                                partners: community businesses, organizations and governments. Volunteer organizers set
+                                an event location, rally member support and utilize partnership provisions.
+                            </p>
+                            <p>
+                                TrashMob administrators confirm, approve and connect partners with event organizers.
+                                Partners and their form of support are indicated on event registration pages. Then local
+                                teamwork commences! Event organizers and partners coordinate access of supplies,
+                                services and instructions. Partners are selected upon availability and proximity to the
+                                event. Note: Supplied services from a given partner may vary by location/branch.{' '}
+                            </p>
+                        </div>
+                        <div className='col-span-12 sm:col-span-5'>
+                            <img src={Garbage} alt='garbage bags being picked up' className='mt-0 m-auto' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 };
