531a1efc7aefc0ae5ff2dfcff7867653d66a2b52
Joe Beernink <3885965+joebeernink@users.noreply.github.com>
2024-12-14 17:21:47 -0800

    Cleaned up Create Event Steps (#1856)
    * Cleaned up Create Event Steps
    
    * Clean up mobile steps
    
    ---------
    
    Co-authored-by: Joe Beernink <jobee@microsoft.com>

--- TrashMobMobile/Pages/CreateEvent/Step5.xaml.cs
+++ TrashMobMobile/Pages/CreateEvent/Step5.xaml.cs
@@ -1,9 +1,30 @@
 namespace TrashMobMobile.Pages.CreateEvent;
 
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
 public partial class Step5 : BaseStepClass
 {
     public Step5()
     {
         InitializeComponent();
+    }
+    public override void OnNavigated()
+    {
+        base.OnNavigated();
+
+        if (ViewModel?.EventViewModel?.Address?.Location != null)
+        {
+            var mapSpan = new MapSpan(ViewModel.EventViewModel.Address.Location, 0.01, 0.01);
+            litterImagesMap.MoveToRegion(mapSpan);
+        }
+    }
+
+    private async void Pin_InfoWindowClicked(object sender, PinClickedEventArgs e)
+    {
+        var p = (Pin)sender;
+
+        var litterReportId = p.AutomationId;
+        await Shell.Current.GoToAsync($"{nameof(ViewLitterReportPage)}?LitterReportId={litterReportId}");
     }
 }
