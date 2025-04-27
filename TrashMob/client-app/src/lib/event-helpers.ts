fcdcf2e346ed66aaf323239a03e63a117029908d
Dopp <dopp@ddxasia.com>
2025-04-28 00:06:40 +0700

    add hours + minutes

--- TrashMob/client-app/src/lib/event-helpers.ts
+++ TrashMob/client-app/src/lib/event-helpers.ts
@@ -6,5 +6,5 @@ import EventData from '@/components/Models/EventData';
 import moment from 'moment';
 
 export const isCompletedEvent = (event: EventData) => {
-    return moment(event.eventDate).isBefore();
+    return moment(event.eventDate).add(event.durationHours, 'hours').add(event.durationMinutes, 'minutes').isBefore();
 };
