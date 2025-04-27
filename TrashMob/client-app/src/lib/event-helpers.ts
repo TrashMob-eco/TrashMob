d594111a9d884c9c61d972de984926adeb14616f
Dopp <dopp@ddxasia.com>
2025-04-27 12:21:28 +0700

    fix: change link to completed event detail to summary

---
+++ TrashMob/client-app/src/lib/event-helpers.ts
@@ -0,0 +1,10 @@
+/**
+ * A utility & helper function for event
+ */
+
+import EventData from '@/components/Models/EventData';
+import moment from 'moment';
+
+export const isCompletedEvent = (event: EventData) => {
+    return moment(event.eventDate).isBefore();
+};
