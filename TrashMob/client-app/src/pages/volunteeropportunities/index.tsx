306af2c7659bf6afe1b07a6f28c2350b2efff53a
Pitipat Srichairat <pitipat.dop@gmail.com>
2025-07-19 05:14:30 +0700

    feat: volunteer-opportunities page fetch from api (#2124)
    Co-authored-by: Dopp <dopp@ddxasia.com>

--- TrashMob/client-app/src/pages/volunteeropportunities/index.tsx
+++ TrashMob/client-app/src/pages/volunteeropportunities/index.tsx
@@ -4,46 +4,16 @@ import { Button } from '@/components/ui/button';
 import { HeroSection } from '@/components/Customization/HeroSection';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+import { useQuery } from '@tanstack/react-query';
+import { GetAllJobOpportunities } from '@/services/job-opportunities';
 
 export const VolunteerOpportunities: React.FC = () => {
-    const opportunities = [
-        {
-            title: 'UX/UI designer',
-            shortDesc: 'Design for mobile app and website',
-            preferredSkill: 'Figma, Responsive Web App Design, Mobile App Design',
-            desc: `
-        We've got a lot of features in the backlog we'd love to get done, but it all starts with
-        design. If you have UX Design skills and want to help shape the website and mobile app
-        as we continue to grow, send us a note, and we'll put your skills to good use!
-      `,
-        },
-        {
-            title: 'Web developer',
-            shortDesc: 'Develop website with React JS and .NETCore',
-            preferredSkill: 'ReactJS, C#, CSS, Azure Maps, AzureAD B2C, Github',
-            desc: `
-        The backlog of features we'd like to get done on the website is huge. Some are critical
-        to the way we do help communities get onboarded to TrashMob.eco. Others add fun
-        components that encourage more and repeated volunteer participation. If you've got the
-        skills to turn UX Designs done in Figma into ReactJS code, or the skills to improve the
-        performance, security, or maintainability of the backend services, we can always use
-        good developers who are passionate about the environment and willing to learn.
-      `,
-        },
-        {
-            title: 'Mobile developer',
-            shortDesc: 'Develop mobile app with .NET MAUI',
-            preferredSkill: '.NET MAUI, Mobile Deployments to Apple Store and Google Play Store, C#',
-            desc: `
-        We're actively working on a TrashMob.eco mobile app, and could use more developers who
-        know their way around building, testing, and deploying mobile app to the Apple and
-        Google Play stores. We've got lots of cool features we want to add to the app, and need
-        help getting all this done. If you have .NET MAUI skills, or want to learn, this is a
-        great opportunity for developers passionate about the environment to dig in and lend a
-        hand!
-      `,
-        },
-    ];
+    const { data: opportunities } = useQuery({
+        queryKey: GetAllJobOpportunities().key,
+        queryFn: GetAllJobOpportunities().service,
+        select: (res) => res.data,
+        enabled: true,
+    });
 
     return (
         <div className='tailwind'>
@@ -52,13 +22,13 @@ export const VolunteerOpportunities: React.FC = () => {
                 <div className='grid grid-cols-12 gap-4'>
                     <div className='col-span-12'>
                         <div className='flex justify-between items-center my-4'>
-                            <h1 className='m-0'>Open volunteer positions (4)</h1>
+                            <h1 className='m-0'>Open volunteer positions ({(opportunities || []).length})</h1>
                             <Button asChild>
                                 <Link to='/contactus'>Contact us</Link>
                             </Button>
                         </div>
                     </div>
-                    <div className='col-span-12 lg:col-span-4'>
+                    <div className='col-span-12 lg:col-span-4 mb-8'>
                         <Card>
                             <CardHeader>
                                 <CardTitle>Looking to contribute to the growth of TrashMob.eco?</CardTitle>
@@ -80,28 +50,33 @@ export const VolunteerOpportunities: React.FC = () => {
                         </Card>
                     </div>
                     <div className='col-span-12 lg:col-span-8'>
-                        {opportunities.map((opp, index) => (
-                            <Card className='mb-4' key={`opportunity-${index}`}>
-                                <CardHeader>
-                                    <CardTitle>{opp.title}</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <p>{opp.shortDesc}</p>
-                                    <p>Preferred skills: {opp.preferredSkill}</p>
-                                    <Collapsible className='group'>
-                                        <CollapsibleContent>{opp.desc}</CollapsibleContent>
-                                        <CollapsibleTrigger>
-                                            <span className='text-primary group-data-[state=open]:hidden'>
-                                                See more
-                                            </span>
-                                            <span className='text-primary group-data-[state=closed]:hidden'>
-                                                See less
-                                            </span>
-                                        </CollapsibleTrigger>
-                                    </Collapsible>
-                                </CardContent>
-                            </Card>
-                        ))}
+                        {opportunities && opportunities.length > 0 ? (
+                            <>
+                                {opportunities.map((opp, index) => (
+                                    <Card className='mb-4' key={`opportunity-${index}`}>
+                                        <CardHeader>
+                                            <CardTitle>{opp.title}</CardTitle>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <p>{opp.tagLine}</p>
+                                            <Collapsible className='group'>
+                                                <CollapsibleContent>{opp.fullDescription}</CollapsibleContent>
+                                                <CollapsibleTrigger>
+                                                    <span className='text-primary group-data-[state=open]:hidden'>
+                                                        See more
+                                                    </span>
+                                                    <span className='text-primary group-data-[state=closed]:hidden'>
+                                                        See less
+                                                    </span>
+                                                </CollapsibleTrigger>
+                                            </Collapsible>
+                                        </CardContent>
+                                    </Card>
+                                ))}
+                            </>
+                        ) : (
+                            <div className='flex justify-center py-24'>There is no opening right now.</div>
+                        )}
                     </div>
                 </div>
             </div>
